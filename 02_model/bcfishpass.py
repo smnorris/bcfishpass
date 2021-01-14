71ba52f55583f6ac3760cf9ce6ecc2d9b3edb8d5
Simon Norris <snorris@hillcrestgeo.ca>
2021-01-13 21:29:13 -0800

    autoformat

--- 02_model/bcfishpass.py
+++ 02_model/bcfishpass.py
@@ -25,7 +25,7 @@ def execute_parallel(sql, wsg):
 
 def read_file(path_string):
     p = Path(path_string)
-    with open(p, mode='r') as f:
+    with open(p, mode="r") as f:
         return f.read()
 
 
@@ -34,7 +34,8 @@ def create_indexes(table):
     """
     db = pgdata.connect()
     schema, table = db.parse_table_name(table)
-    db.execute(f"""CREATE INDEX ON {schema}.{table} (linear_feature_id);
+    db.execute(
+        f"""CREATE INDEX ON {schema}.{table} (linear_feature_id);
     CREATE INDEX ON {schema}.{table} (blue_line_key);
     CREATE INDEX ON {schema}.{table} (watershed_group_code);
     CREATE INDEX ON {schema}.{table} USING GIST (wscode_ltree);
@@ -42,7 +43,8 @@ def create_indexes(table):
     CREATE INDEX ON {schema}.{table} USING GIST (localcode_ltree);
     CREATE INDEX ON {schema}.{table} USING BTREE (localcode_ltree);
     CREATE INDEX ON {schema}.{table} USING GIST (geom);
-    """)
+    """
+    )
 
 
 @click.group()
@@ -57,19 +59,32 @@ def cli():
 @click.argument("id_b")
 @click.argument("downstream_ids_col")
 @click.option("--include_equivalent_measure", default=False, is_flag=True)
-def add_downstream_ids(table_a, id_a, table_b, id_b, downstream_ids_col, include_equivalent_measure):
+def add_downstream_ids(
+    table_a, id_a, table_b, id_b, downstream_ids_col, include_equivalent_measure
+):
     """note downstream ids
     """
     db = pgdata.connect()
     schema_a, table_a = db.parse_table_name(table_a)
     schema_b, table_b = db.parse_table_name(table_b)
     # ensure that any existing values get removed
-    db.execute(f"ALTER TABLE {schema_a}.{table_a} DROP COLUMN IF EXISTS {downstream_ids_col}")
+    db.execute(
+        f"ALTER TABLE {schema_a}.{table_a} DROP COLUMN IF EXISTS {downstream_ids_col}"
+    )
     temp_table = table_a + "_tmp"
     db[f"{schema_a}.{temp_table}"].drop()
     db.execute(f"CREATE TABLE {schema_a}.{temp_table} (LIKE {schema_a}.{table_a})")
-    db.execute(f"ALTER TABLE {schema_a}.{temp_table} ADD COLUMN {downstream_ids_col} integer[]")
-    groups = sorted([g[0] for g in db.query(f"SELECT DISTINCT watershed_group_CODE from {schema_a}.{table_a}")])
+    db.execute(
+        f"ALTER TABLE {schema_a}.{temp_table} ADD COLUMN {downstream_ids_col} integer[]"
+    )
+    groups = sorted(
+        [
+            g[0]
+            for g in db.query(
+                f"SELECT DISTINCT watershed_group_CODE from {schema_a}.{table_a}"
+            )
+        ]
+    )
     # todo - is this really the best way to specify which query to use?
     if include_equivalent_measure:
         q = "sql/00_add_downstream_and_equivalent_ids.sql"
@@ -83,7 +98,7 @@ def add_downstream_ids(table_a, id_a, table_b, id_b, downstream_ids_col, include
         table_b=sql.Identifier(table_b),
         id_a=sql.Identifier(id_a),
         id_b=sql.Identifier(id_b),
-        dnstr_ids_col=sql.Identifier(downstream_ids_col)
+        dnstr_ids_col=sql.Identifier(downstream_ids_col),
     )
     # run each group in parallel
     func = partial(execute_parallel, query)
@@ -103,7 +118,7 @@ def add_downstream_ids(table_a, id_a, table_b, id_b, downstream_ids_col, include
     cur = conn.cursor()
     cur.execute(f"VACUUM ANALYZE {schema_a}.{table_a}")
     # downstream ids are stored as intarray, we could index them
-    #db.execute(f"CREATE INDEX ON {schema_a}.{table_a} USING GIST ({downstream_ids_col} gist__int_ops);")
+    # db.execute(f"CREATE INDEX ON {schema_a}.{table_a} USING GIST ({downstream_ids_col} gist__int_ops);")
 
 
 @cli.command()
@@ -119,12 +134,23 @@ def add_upstream_ids(table_a, id_a, table_b, id_b, upstream_ids_col):
     schema_a, table_a = db.parse_table_name(table_a)
     schema_b, table_b = db.parse_table_name(table_b)
     # ensure that any existing values get removed
-    db.execute(f"ALTER TABLE {schema_a}.{table_a} DROP COLUMN IF EXISTS {upstream_ids_col}")
+    db.execute(
+        f"ALTER TABLE {schema_a}.{table_a} DROP COLUMN IF EXISTS {upstream_ids_col}"
+    )
     temp_table = table_a + "_tmp"
     db[f"{schema_a}.{temp_table}"].drop()
     db.execute(f"CREATE TABLE {schema_a}.{temp_table} (LIKE {schema_a}.{table_a})")
-    db.execute(f"ALTER TABLE {schema_a}.{temp_table} ADD COLUMN {upstream_ids_col} integer[]")
-    groups = sorted([g[0] for g in db.query(f"SELECT DISTINCT watershed_group_code from {schema_a}.{table_a}")])
+    db.execute(
+        f"ALTER TABLE {schema_a}.{temp_table} ADD COLUMN {upstream_ids_col} integer[]"
+    )
+    groups = sorted(
+        [
+            g[0]
+            for g in db.query(
+                f"SELECT DISTINCT watershed_group_code from {schema_a}.{table_a}"
+            )
+        ]
+    )
     query = sql.SQL(read_file("sql/00_add_upstream_ids.sql")).format(
         schema_a=sql.Identifier(schema_a),
         schema_b=sql.Identifier(schema_b),
@@ -133,7 +159,7 @@ def add_upstream_ids(table_a, id_a, table_b, id_b, upstream_ids_col):
         table_b=sql.Identifier(table_b),
         id_a=sql.Identifier(id_a),
         id_b=sql.Identifier(id_b),
-        upstr_ids_col=sql.Identifier(upstream_ids_col)
+        upstr_ids_col=sql.Identifier(upstream_ids_col),
     )
     # run each group in parallel
     func = partial(execute_parallel, query)
@@ -167,12 +193,17 @@ def segment_streams(stream_table, point_table):
     db = pgdata.connect()
     stream_schema, stream_table = db.parse_table_name(stream_table)
     point_schema, point_table = db.parse_table_name(point_table)
-    groups = [g[0] for g in db.query(f"SELECT DISTINCT watershed_group_CODE FROM bcfishpass.streams")]
+    groups = [
+        g[0]
+        for g in db.query(
+            "SELECT DISTINCT watershed_group_code FROM bcfishpass.streams"
+        )
+    ]
     query = sql.SQL(read_file("sql/00_segment_streams.sql")).format(
         stream_schema=sql.Identifier(stream_schema),
         stream_table=sql.Identifier(stream_table),
         point_schema=sql.Identifier(point_schema),
-        point_table=sql.Identifier(point_table)
+        point_table=sql.Identifier(point_table),
     )
     func = partial(execute_parallel, query)
     n_processes = multiprocessing.cpu_count() - 1
@@ -197,7 +228,7 @@ def report(point_table, point_id, barriers_table, dnstr_barriers_id):
         point_id=sql.Identifier(point_id),
         barriers_schema=sql.Identifier(barriers_schema),
         barriers_table=sql.Identifier(barriers_table),
-        dnstr_barriers_id=sql.Identifier(dnstr_barriers_id)
+        dnstr_barriers_id=sql.Identifier(dnstr_barriers_id),
     )
     conn = db.engine.raw_connection()
     cur = conn.cursor()
