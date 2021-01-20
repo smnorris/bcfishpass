91bc4251f59c88d7ef8e1053b5897489a40b140a
Simon Norris <snorris@hillcrestgeo.ca>
2021-01-20 15:28:30 -0800

    add missing other barrier files

---
+++ 01_prep/05_other/other.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# load additional falls
+
+psql -c "CREATE SCHEMA IF NOT EXISTS cwf"
+ogr2ogr -f PostgreSQL \
+  "PG:host=$PGHOST user=$PGUSER dbname=$PGDATABASE port=$PGPORT" \
+  -overwrite \
+  -s_srs EPSG:4326 \
+  -t_srs EPSG:3005 \
+  -lco GEOMETRY_NAME=geom \
+  -nln cwf.waterfalls_additional \
+  data/falls.geojson \
+  falls
+
+# match the dams to streams
+psql -f sql/cwf_waterfalls_additional.sql
+
+# load misc barriers
+psql -f sql/misc_barriers.sql
