d5b9e2b82565f127d1a52b660d65927364e0fef5
Simon Norris <snorris@hillcrestgeo.ca>
2021-02-08 10:58:08 -0800

    add missing gradienjt_barriers_passable query

---
+++ 01_prep/05_other/sql/gradient_barriers_passable.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS bcfishpass.gradient_barriers_passable;
+
+CREATE TABLE bcfishpass.gradient_barriers_passable
+(blue_line_key integer,
+downstream_route_measure double precision,
+watershed_group_code text,
+reviewer text,
+notes text);
+
+ALTER TABLE bcfishpass.gradient_barriers_passable
+ADD PRIMARY KEY (blue_line_key, downstream_route_measure);
