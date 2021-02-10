187c05620c585871057217a29b6d1ad901187dcf
Simon Norris <snorris@hillcrestgeo.ca>
2021-02-10 13:58:08 -0800

    make extent report easier to decipher

--- 04_reporting/sql/wcrp_barrier_extent.sql
+++ 04_reporting/sql/wcrp_barrier_extent.sql
@@ -2,17 +2,17 @@
 SELECT
   watershed_group_code,
   wcrp_barrier_type,
-  ROUND(SUM(ch_spawning_belowupstrbarriers_km)::numeric, 2) as ch_spawning_belowupstrbarriers_km,
-  ROUND(SUM(ch_rearing_belowupstrbarriers_km)::numeric, 2) as ch_rearing_belowupstrbarriers_km,
-  ROUND(SUM(co_spawning_belowupstrbarriers_km)::numeric, 2) as co_spawning_belowupstrbarriers_km,
-  ROUND(SUM(co_rearing_belowupstrbarriers_km)::numeric, 2) as co_rearing_belowupstrbarriers_km,
-  ROUND(SUM(sk_spawning_belowupstrbarriers_km)::numeric, 2) as sk_spawning_belowupstrbarriers_km,
-  ROUND(SUM(sk_rearing_belowupstrbarriers_km)::numeric, 2) as sk_rearing_belowupstrbarriers_km,
-  ROUND(SUM(st_spawning_belowupstrbarriers_km)::numeric, 2) as st_spawning_belowupstrbarriers_km,
-  ROUND(SUM(st_rearing_belowupstrbarriers_km)::numeric, 2) as st_rearing_belowupstrbarriers_km,
-  ROUND(SUM(all_spawning_belowupstrbarriers_km)::numeric, 2) as all_spawning_belowupstrbarriers_km,
-  ROUND(SUM(all_rearing_belowupstrbarriers_km)::numeric, 2) as all_rearing_belowupstrbarriers_km,
-  ROUND(SUM(all_spawningrearing_belowupstrbarriers_km)::numeric, 2) as all_spawningrearing_belowupstrbarriers_km
+  ROUND(SUM(ch_spawning_belowupstrbarriers_km)::numeric, 2) as ch_spawning_blocked_km,
+  ROUND(SUM(ch_rearing_belowupstrbarriers_km)::numeric, 2) as ch_rearing_blocked_km,
+  ROUND(SUM(co_spawning_belowupstrbarriers_km)::numeric, 2) as co_spawning_blocked_km,
+  ROUND(SUM(co_rearing_belowupstrbarriers_km)::numeric, 2) as co_rearing_blocked_km,
+  ROUND(SUM(sk_spawning_belowupstrbarriers_km)::numeric, 2) as sk_spawning_blocked_km,
+  ROUND(SUM(sk_rearing_belowupstrbarriers_km)::numeric, 2) as sk_rearing_blocked_km,
+  ROUND(SUM(st_spawning_belowupstrbarriers_km)::numeric, 2) as st_spawning_blocked_km,
+  ROUND(SUM(st_rearing_belowupstrbarriers_km)::numeric, 2) as st_rearing_blocked_km,
+  ROUND(SUM(all_spawning_belowupstrbarriers_km)::numeric, 2) as all_spawning_blocked_km,
+  ROUND(SUM(all_rearing_belowupstrbarriers_km)::numeric, 2) as all_rearing_blocked_km,
+  ROUND(SUM(all_spawningrearing_belowupstrbarriers_km)::numeric, 2) as all_spawningrearing_blocked_km
 FROM bcfishpass.crossings
 WHERE watershed_group_code in ('BULK','LNIC')
 GROUP BY
