35b570d7f0edc2a067d4611e15e8ab2a785a8658
Simon Norris <snorris@hillcrestgeo.ca>
2021-03-12 12:36:16 -0800

    add dnstr_barriers_anthropogenic_count column

--- 02_model/model.sh
+++ 02_model/model.sh
@@ -231,6 +231,11 @@ python bcfishpass.py report bcfishpass.crossings aggregated_crossings_id bcfishp
 psql -c "COMMENT ON COLUMN bcfishpass.crossings.dnstr_crossings IS 'List of the aggregated_crossings_id values of crossings downstream of the given crossing, in order downstream';"
 psql -c "COMMENT ON COLUMN bcfishpass.crossings.dnstr_barriers_anthropogenic IS 'List of the aggregated_crossings_id values of barrier crossings downstream of the given crossing, in order downstream';"
 
+# also note the number of barriers downstream, just a count of values in dnstr_barriers_anthropogenic
+psql -c "ALTER TABLE bcfishpass.crossings ADD COLUMN dnstr_barriers_anthropogenic_count integer"
+psql -c "COMMENT ON COLUMN bcfishpass.crossings.dnstr_barriers_anthropogenic_count IS 'A count of the barrier crossings downstream of the given crossing';"
+psql -c "UPDATE bcfishpass.crossings SET dnstr_barriers_anthropogenic_count = array_length(dnstr_barriers_anthropogenic, 1) WHERE dnstr_barriers_anthropogenic IS NOT NULL";
+
 # populating the belowupstrbarriers for OBS in the crossings table requires a separate query
 psql -f sql/00_report_crossings_obs_belowupstrbarriers.sql
 
