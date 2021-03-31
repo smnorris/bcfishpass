f20ce9be0d1c4e1c93f812077fcc27a986ab1a57
Simon Norris <snorris@hillcrestgeo.ca>
2021-03-31 15:38:25 -0700

    add missing colons to urls

--- 03_mapping/wcrp_pdfs/list_tiles.sh
+++ 03_mapping/wcrp_pdfs/list_tiles.sh
@@ -4,7 +4,7 @@ echo '# Fish Passage 48"x36" pdfs' > README.md
 
 echo '## Bulkley River (BULK)' >> README.md
 psql -t -c "SELECT
-  '- [FishPassage_'||map_tile_display_name||'.pdf](https//cwfwcrp.s3-us-west-1.amazonaws.com/FishPassage_'||map_tile_display_name||'.pdf)'
+  '- [FishPassage_'||map_tile_display_name||'.pdf](https://cwfwcrp.s3-us-west-1.amazonaws.com/FishPassage_'||map_tile_display_name||'.pdf)'
   FROM whse_basemapping.dbm_mof_50k_grid g
   INNER JOIN whse_basemapping.fwa_watershed_groups_poly w
   ON st_intersects(g.geom, w.geom)
@@ -13,7 +13,7 @@ psql -t -c "SELECT
 
 echo '## Horsefly River (HORS)' >> README.md
 psql -t -c "SELECT
-  '- [FishPassage_'||map_tile_display_name||'.pdf](https//cwfwcrp.s3-us-west-1.amazonaws.com/FishPassage_'||map_tile_display_name||'.pdf)'
+  '- [FishPassage_'||map_tile_display_name||'.pdf](https://cwfwcrp.s3-us-west-1.amazonaws.com/FishPassage_'||map_tile_display_name||'.pdf)'
   FROM whse_basemapping.dbm_mof_50k_grid g
   INNER JOIN whse_basemapping.fwa_watershed_groups_poly w
   ON st_intersects(g.geom, w.geom)
@@ -22,7 +22,7 @@ psql -t -c "SELECT
 
 echo '## Lower Nicola River (LNIC)' >> README.md
 psql -t -c "SELECT
-  '- [FishPassage_'||map_tile_display_name||'.pdf](https//cwfwcrp.s3-us-west-1.amazonaws.com/FishPassage_'||map_tile_display_name||'.pdf)'
+  '- [FishPassage_'||map_tile_display_name||'.pdf](https://cwfwcrp.s3-us-west-1.amazonaws.com/FishPassage_'||map_tile_display_name||'.pdf)'
   FROM whse_basemapping.dbm_mof_50k_grid g
   INNER JOIN whse_basemapping.fwa_watershed_groups_poly w
   ON st_intersects(g.geom, w.geom)
