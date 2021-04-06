4606086faa4581bc80815cfb966773bf15c81bee
Simon Norris <snorris@hillcrestgeo.ca>
2021-04-05 21:27:35 -0700

    add major dams to crossings table and report on DCI in ELKR

--- 04_reporting/report.sh
+++ 04_reporting/report.sh
@@ -4,3 +4,6 @@ set -euxo pipefail
 psql2csv < sql/watershed_summary.sql > reports/watershed_summary.csv
 psql2csv < sql/wcrp_barrier_extent.sql > reports/wcrp_barrier_extent.csv
 psql2csv < sql/wcrp_barrier_severity.sql > reports/wcrp_barrier_severity.csv
+
+# this one is rather basic, just a single value
+psql2csv < sql/wcrp_wct_dci.sql > reports/wcrp_wct_dci.csv
---
+++ 04_reporting/sql/wcrp_wct_dci.sql
@@ -0,0 +1,33 @@
+-- calculate DCI for WCT watershed(s)
+
+-- First, find the lengths of each portion of stream between barriers/potential barriers
+WITH segments AS
+(
+  SELECT
+    watershed_group_code,
+    dnstr_barriers_anthropogenic,
+    SUM(ST_Length(geom)) as length_segment
+  FROM bcfishpass.streams
+  WHERE accessibility_model_wct IS NOT NULL
+  AND wscode_ltree <@ '300.602565.854327.993941.902282.132363'::ltree IS FALSE  -- exclude the flathead
+  GROUP BY watershed_group_code, dnstr_barriers_anthropogenic
+),
+
+-- find the total length of all streams selected above
+totals AS
+(
+  SELECT
+   watershed_group_code,
+   sum(length_segment) as length_total
+  FROM segments
+  GROUP BY watershed_group_code
+)
+
+-- calculate DCI
+SELECT
+  t.watershed_group_code,
+  ROUND(SUM((s.length_segment * s.length_segment) / (t.length_total * t.length_total))::numeric, 4) as dci
+FROM segments s
+INNER JOIN totals t
+ON s.watershed_Group_code = t.watershed_group_code
+GROUP BY t.watershed_group_code;
