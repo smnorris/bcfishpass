927ed73746845bd5dd70bb30028ca499b8688f1a
Simon Norris <snorris@hillcrestgeo.ca>
2021-04-14 10:39:41 -0700

    remove temp comments and just call all misc stuff in model_habitat for now

--- 02_model/model_access.sh
+++ 02_model/model_access.sh
@@ -14,24 +14,21 @@ psql -f sql/utmzone.sql
 # Load data (QA/fixes/latest PSCIS)
 # -----------
 # - modelled crossings fixes
-# psql -c "DROP TABLE IF EXISTS bcfishpass.modelled_stream_crossings_fixes"
-# psql -c "CREATE TABLE bcfishpass.modelled_stream_crossings_fixes (modelled_crossing_id integer PRIMARY KEY, structure text, watershed_group_code text, reviewer text, notes text)"
-# psql -c "\copy bcfishpass.modelled_stream_crossings_fixes FROM '../01_prep/01_modelled_stream_crossings/data/modelled_stream_crossings_fixes.csv' delimiter ',' csv header"
-# psql -c "CREATE INDEX ON bcfishpass.modelled_stream_crossings_fixes (modelled_crossing_id)"
-#
-# # - load pscis / pscis fixes and re-run scripts matching PSCIS points to streams
-# cd ../01_prep/02_pscis
-# ./load.sh
-# ./pscis.sh
-# cd ../../02_model
-#
-# # - and falls, misc
-# cd ../01_prep/06_falls
-# ./falls.sh
-# cd ../07_misc
-# ./misc.sh
-# cd ../../02_model
-#
+psql -c "DROP TABLE IF EXISTS bcfishpass.modelled_stream_crossings_fixes"
+psql -c "CREATE TABLE bcfishpass.modelled_stream_crossings_fixes (modelled_crossing_id integer PRIMARY KEY, structure text, watershed_group_code text, reviewer text, notes text)"
+psql -c "\copy bcfishpass.modelled_stream_crossings_fixes FROM '../01_prep/01_modelled_stream_crossings/data/modelled_stream_crossings_fixes.csv' delimiter ',' csv header"
+psql -c "CREATE INDEX ON bcfishpass.modelled_stream_crossings_fixes (modelled_crossing_id)"
+# - load pscis / pscis fixes and re-run scripts matching PSCIS points to streams
+cd ../01_prep/02_pscis
+./load.sh
+./pscis.sh
+cd ../../02_model
+# - and falls, misc
+cd ../01_prep/06_falls
+./falls.sh
+cd ../07_misc
+./misc.sh
+cd ../../02_model
 
 # -----------
 # Create barrier tables, run access model
