b23f3e43a398da5df0724021b60c2be98c96a2d9
Simon Norris <snorris@hillcrestgeo.ca>
2021-04-14 11:39:25 -0700

    include measured 1st order CW in streams table (unused but maybe handy)

--- 02_model/sql/load_channel_width.sql
+++ 02_model/sql/load_channel_width.sql
@@ -1,27 +1,51 @@
--- add modelled channel width column to streams table
+-- add channel width column to streams table (both measured and modelled)
 ALTER TABLE bcfishpass.streams ADD COLUMN IF NOT EXISTS channel_width double precision;
 
--- load channel width to streams table from the measured/modelled data where a model has been run
+-- first, load all *measured* channel width values to streams table where available
 WITH cw AS
 (SELECT DISTINCT
   s.wscode_ltree,
   s.localcode_ltree,
   s.linear_feature_id,
-  COALESCE(cw1.channel_width_measured, cw2.channel_width_modelled) as channel_width,
+  c.channel_width_measured AS channel_width,
   s.edge_type
 FROM bcfishpass.streams s
-LEFT OUTER JOIN bcfishpass.channel_width_measured cw1
-ON s.wscode_ltree = cw1.wscode_ltree
-AND s.localcode_ltree = cw1.localcode_ltree
-LEFT OUTER JOIN bcfishpass.channel_width_modelled cw2
-ON s.wscode_ltree = cw2.wscode_ltree
-AND s.localcode_ltree = cw2.localcode_ltree
+LEFT OUTER JOIN bcfishpass.channel_width_measured c
+ON s.wscode_ltree = c.wscode_ltree
+AND s.localcode_ltree = c.localcode_ltree
 LEFT OUTER JOIN whse_basemapping.fwa_waterbodies wb
 ON s.waterbody_key = wb.waterbody_key
+-- only apply channel width to rivers and streams
+AND (wb.waterbody_type = 'R' OR (wb.waterbody_type IS NULL AND s.edge_type IN (1000,1100,2000,2300)))
+)
+
+UPDATE bcfishpass.streams s
+SET
+  channel_width = cw.channel_width
+FROM cw
+WHERE s.linear_feature_id = cw.linear_feature_id;
+
+
+-- now load all *modelled* channel width values to streams table
+-- (note that this will not include first order streams)
+WITH cw AS
+(SELECT DISTINCT
+  s.wscode_ltree,
+  s.localcode_ltree,
+  s.linear_feature_id,
+  c.channel_width_modelled AS channel_width,
+  s.edge_type
+FROM bcfishpass.streams s
+LEFT OUTER JOIN bcfishpass.channel_width_modelled c
+ON s.wscode_ltree = c.wscode_ltree
+AND s.localcode_ltree = c.localcode_ltree
+LEFT OUTER JOIN whse_basemapping.fwa_waterbodies wb
+ON s.waterbody_key = wb.waterbody_key
+-- restrict to watersheds where we are running the model
+-- (although this should be the only place where there is data)
 INNER JOIN bcfishpass.param_watersheds wsg
 ON s.watershed_group_code = wsg.watershed_group_code
 WHERE wsg.model = 'cw'
--- don't model channel width on first order streams
 AND s.stream_order > 1
 -- only apply channel width to rivers and streams
 AND (wb.waterbody_type = 'R' OR (wb.waterbody_type IS NULL AND s.edge_type IN (1000,1100,2000,2300)))
