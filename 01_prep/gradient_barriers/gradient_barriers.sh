c5c1bc85e291657e083d9e17d16bbca63b5efe7f
Simon Norris <snorris@hillcrestgeo.ca>
2021-05-04 15:44:59 -0700

    process gradient in all watershed groups

--- 01_prep/gradient_barriers/gradient_barriers.sh
+++ 01_prep/gradient_barriers/gradient_barriers.sh
@@ -12,10 +12,10 @@ psql -c "CREATE TABLE bcfishpass.gradient_barriers_test
         )"
 
 # Load as loop through watershed groups
+# Only process a select few for now
 for WSG in $(psql -t -P border=0,footer=no \
   -c "SELECT watershed_group_code
       FROM whse_basemapping.fwa_watershed_groups_poly
-      WHERE watershed_group_code IN ('VICT','COWN','SANJ','SQAM','LARL','KOTL','ADMS','LNIC','BULK','HORS','ELKR')
       ORDER BY watershed_group_code")
 do
   psql -X -v wsg="$WSG" < sql/gradient_barriers.sql
@@ -23,29 +23,3 @@ done
 
 psql -c "CREATE INDEX ON bcfishpass.gradient_barriers_test (blue_line_key)"
 psql -c "CREATE INDEX ON bcfishpass.gradient_barriers_test (blue_line_key, downstream_route_measure)"
-
-
-# for qa
-# psql -c "Drop table if exists temp.gradient_barriers_test ;
-#
-# create table temp.gradient_barriers_test
-    # (id serial primary key,
-        # gradient_class integer,
-        # watershed_group_code text,
-        # geom geometry(point, 3005));
-#
-# insert into temp.gradient_barriers_test
-# (gradient_class, watershed_group_code, geom)
-# SELECT
- # g.gradient_class,
- # s.watershed_group_code,
- # ST_Force2D((ST_Dump(ST_Locatealong(s.geom, g.downstream_route_measure))).geom)::geometry(Point,3005) as geom
- # FROM bcfishpass.gradient_barriers_test g
-# INNER JOIN whse_basemapping.fwa_stream_networks_sp s
-  # ON g.blue_line_key = s.blue_line_key AND
-     # g.downstream_route_measure >= s.downstream_route_measure AND
-     # g.downstream_route_measure < s.upstream_route_measure
-  # WHERE s.watershed_group_code in ('VICT','COWN','SANJ','SQAM','LARL','KOTL','ADMS','LNIC','BULK','HORS','ELKR');
-#
-# create index on temp.gradient_barriers_test USING gist (geom);"
-#
