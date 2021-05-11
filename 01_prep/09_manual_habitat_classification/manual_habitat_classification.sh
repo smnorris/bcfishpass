896dae6ff84786846d38f31cb1c8ac226596f6cd
Simon Norris <snorris@hillcrestgeo.ca>
2021-05-11 09:38:12 -0700

    make sure upstream measure > dnstr measure in manual habitat table

--- 01_prep/09_manual_habitat_classification/manual_habitat_classification.sh
+++ 01_prep/09_manual_habitat_classification/manual_habitat_classification.sh
@@ -2,11 +2,11 @@
 set -euxo pipefail
 
 # create the table
-psql -c "DROP TABLE IF EXISTS bcfishpass.manual_habitat_classification"
-psql -c "CREATE TABLE bcfishpass.manual_habitat_classification
-  (blue_line_key integer,
+psql -c "DROP TABLE IF EXISTS bcfishpass.manual_habitat_classification CASCADE"
+psql -c "CREATE TABLE bcfishpass.manual_habitat_classification (
+  blue_line_key integer,
   downstream_route_measure double precision,
-  upstream_route_measure double precision,
+  upstream_route_measure double precision CHECK (upstream_route_measure > downstream_route_measure),
   species_code text,
   habitat_type text,
   habitat_ind boolean,
@@ -14,7 +14,8 @@ psql -c "CREATE TABLE bcfishpass.manual_habitat_classification
   watershed_group_code varchar(4),
   source text,
   notes text,
-  PRIMARY KEY (blue_line_key, downstream_route_measure,species_code,habitat_type))"
+  PRIMARY KEY (blue_line_key, downstream_route_measure,species_code,habitat_type)
+  )"
 
 # load the data
 psql -c "\copy bcfishpass.manual_habitat_classification FROM 'data/manual_habitat_classification.csv' delimiter ',' csv header"
