bca1c2be78f63d9662a40c9b268a389906f19aff
Simon Norris <snorris@hillcrestgeo.ca>
2021-06-14 18:11:08 -0700

    fix syntax errors in qgis lyr file symbology filters

--- 01_prep/habitat/01_discharge/discharge_load.sh
+++ 01_prep/habitat/01_discharge/discharge_load.sh
@@ -9,8 +9,8 @@ raster2pgsql data/discharge.tif bcfishpass.discharge_raster | psql
 psql -c "drop table if exists bcfishpass.discharge_load"
 psql -c "create table bcfishpass.discharge_load (watershed_feature_id integer primary key, discharge_mm double precision);"
 
-# load all Fraser/Peace/Columbia watershed groups in parallel to temp postgres table
-psql -A -t -P border=0,footer=off -c "SELECT DISTINCT watershed_group_code
+# load all PCIC study area watershed groups in parallel to temp postgres table
+psql -AtX -P border=0,footer=off -c "SELECT DISTINCT watershed_group_code
       FROM whse_basemapping.fwa_assessment_watersheds_poly
       WHERE (wscode_ltree <@ '100'::ltree OR wscode_ltree <@ '300'::ltree OR wscode_ltree <@ '200'::ltree)
       ORDER BY watershed_group_code" |
