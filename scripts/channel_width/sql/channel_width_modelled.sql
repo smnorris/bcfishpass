082b16dc18c2b06400e5b8b4fca5eafc031093f4
Simon Norris <snorris@hillcrestgeo.ca>
2022-01-17 21:29:46 -0800

    tidy

--- scripts/channel_width/sql/channel_width_modelled.sql
+++ scripts/channel_width/sql/channel_width_modelled.sql
@@ -56,9 +56,8 @@ SELECT
   --    (EXP(-2.2383120 + 0.3121556 * ln(s.upstream_area_ha / 100) + 0.6546995 * ln(p.map_upstream / 10))
   --  )::numeric, 2
   --)
-  -- updated formula, 2022
-  --round(exp(0.3071 + 0.4578 * (ln(s.upstream_area_ha / 100) + ln((coalesce(p.map_upstream, 0) + 1) / 1000)))::numeric, 2) as channel_width_modelled
-  round(exp(0.3071 + 0.4578 * (ln(s.upstream_area_ha) + ln(p.map_upstream) - ln(100) - ln(1000)))) as channel_width_modelled
+  -- updated formula, Thorley and Irvine 2021b
+  round(exp(0.3071 + 0.4578 * (ln(s.upstream_area_ha) + ln(coalesce(p.map_upstream, 0) + 1)) - ln(100) - ln(1000)))) as channel_width_modelled
 FROM streams s
 INNER JOIN bcfishpass.mean_annual_precip p
 ON s.wscode_ltree = p.wscode_ltree
