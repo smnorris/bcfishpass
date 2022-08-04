18920c5c3bd30417a9c441dba5aa9aad0d027f53
Simon Norris <snorris@hillcrestgeo.ca>
2022-08-04 13:06:50 -0700

    load cabd dams

--- scripts/dams/dams.sh
+++ scripts/dams/dams.sh
@@ -10,16 +10,17 @@ PSQL_CMD="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 # ---------
 # download CWF dam data and match to FWA streams
 # ---------
+$PSQL_CMD -c "create schema if not exists cabd"
+
 ogr2ogr -f PostgreSQL \
   "PG:$DATABASE_URL" \
   -overwrite \
   -s_srs EPSG:4326 \
   -t_srs EPSG:3005 \
   -lco GEOMETRY_NAME=geom \
-  -lco FID=bcdams_id \
-  -nln bcfishpass.cwf_bcdams \
-  /vsicurl/https://raw.githubusercontent.com/smnorris/bcdams/main/bcdams.geojson \
-  bcdams
+  -nln cabd.dams \
+  "https://cabd-web.azurewebsites.net/cabd-api/features/dams?filter=province_territory_code:eq:bc" \
+  OGRGeoJSON
 
 # match the dams to streams
 $PSQL_CMD -f sql/dams.sql
