50c66ec6bd24ee65cbba00f6108a2eac6231d3a0
Simon Norris <snorris@hillcrestgeo.ca>
2022-12-08 20:35:11 -0800

    working script and function for dnstr tables

--- scripts/model_access/upstr_dnstr_ids.py
+++ scripts/model_access/upstr_dnstr_ids.py
@@ -98,6 +98,13 @@ class Database(object):
             with self.conn.cursor() as curs:
                 curs.executemany(sql, params)
 
+    def get_type(self, schema, table, column):
+        """Return type for given schema/table/column"""
+        return self.query("""select data_type from information_schema.columns
+                      where table_schema = {schema}
+                      and table_name = {table}
+                      and column_name = {column}""")[0]
+
 
 def execute_parallel(sql, wsg):
     """Execute sql for specified wsg using a non-pooled, non-parallel conn
@@ -181,6 +188,65 @@ def add_downstream_ids(
         schema_a=sql.Identifier(schema_a),
         schema_b=sql.Identifier(schema_b),
         temp_table=sql.Identifier(temp_table),
+        table_a=sql.Identifier(table_a),
+        table_b=sql.Identifier(table_b),
+        id_a=sql.Identifier(id_a),
+        id_b=sql.Identifier(id_b),
+        dnstr_ids_col=sql.Identifier(downstream_ids_col),
+    )
+    # run each group in parallel
+    func = partial(execute_parallel, query)
+    n_processes = multiprocessing.cpu_count() - 1
+    pool = multiprocessing.Pool(processes=n_processes)
+    pool.map(func, groups)
+    pool.close()
+    pool.join()
+    # drop source table, rename new table, re-create indexes
+    db.execute(f"DROP TABLE IF EXISTS {schema_a}.{table_a}")
+    db.execute(f"ALTER TABLE {schema_a}.{temp_table} RENAME TO {table_a}")
+    create_indexes(f"{schema_a}.{table_a}")
+    db.execute(f"ALTER TABLE {schema_a}.{table_a} ADD PRIMARY KEY ({id_a})")
+    # make sure the table gets analyzed before running this again
+    db.conn.set_isolation_level(0)
+    cur = db.conn.cursor()
+    cur.execute(f"VACUUM ANALYZE {schema_a}.{table_a}")
+
+
+@cli.command()
+@click.argument("table_a")
+@click.argument("id_a")
+@click.argument("table_b")
+@click.argument("id_b")
+@click.argument("out_table")
+@click.argument("downstream_ids_col")
+@click.option("--include_equivalent_measure", default=False, is_flag=True)
+def add_downstream_ids_2(
+    table_a, id_a, table_b, id_b, out_table, downstream_ids_col, include_equivalent_measure
+):
+    """record aggregated records in table b that are downstream of records in table a
+    """
+    db = Database()
+
+    schema_a, table_a = db.parse_table_name(table_a)
+    schema_b, table_b = db.parse_table_name(table_b)
+    # ensure table does not already exist
+    db.execute(f"drop table if exists {out_table}")
+    id_a_type = db.get_type(schema_a, table_a, id_a)
+    db.execute(f"""create table {out_table} (
+        {id_a} {id_a_type} primary key, 
+        {downstream_ids_col} [text])""")
+    
+    groups = sorted(
+        [
+            g[0]
+            for g in db.query(
+                f"SELECT DISTINCT watershed_group_code from {schema_a}.{table_a}"
+            )
+        ]
+    )
+    query = sql.SQL(read_file(q)).format(
+        schema_a=sql.Identifier(schema_a),
+        schema_b=sql.Identifier(schema_b),
         table_a=sql.Identifier(table_a),
         table_b=sql.Identifier(table_b),
         id_a=sql.Identifier(id_a),
