39ccb3ac9681bafbc63f4b9168366941d88bc953
Simon Norris <snorris@hillcrestgeo.ca>
2023-01-06 10:05:22 -0800

    access model autodetect spp scenario queries

--- scripts/model_access/model_access.sh
+++ scripts/model_access/model_access.sh
@@ -5,6 +5,9 @@ set -euxo pipefail
 PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 WSGS=$($PSQL -AXt -c "SELECT watershed_group_code FROM bcfishpass.param_watersheds")
 
+# look for species models/scenarios to be processed in the sql folder,
+# all files with model_barriers prefix
+MODELS=$(ls sql/model_barriers*.sql | sed -e "s/sql\/model_barriers_//" | sed -e "s/.sql//")
 
 # -----
 # LOAD STREAMS
@@ -17,22 +20,26 @@ $PSQL -c "VACUUM ANALYZE bcfishpass.streams"
 # -----
 # BREAK STREAMS
 # -----
+
 # break at observations
 parallel --jobs 4 --no-run-if-empty \
 	"echo \"SELECT bcfishpass.break_streams(:'point_table', :'wsg');\" | \
 	$PSQL -v wsg={1} -v point_table=observations" ::: $WSGS
+
 # break at crossings 
 parallel --jobs 4 --no-run-if-empty \
 	"echo \"SELECT bcfishpass.break_streams(:'point_table', :'wsg');\" | \
 	$PSQL -v wsg={1} -v point_table=crossings" ::: $WSGS
+
 # break at natural barriers for all given species scenarios
-for BARRIERTYPE in ch_co_sk ch_co_sk_b st wct 
+for BARRIERTYPE in $MODELS
 do
 	parallel --jobs 4 --no-run-if-empty \
 		"echo \"SELECT bcfishpass.break_streams(:'point_table', :'wsg');\" | \
 		$PSQL -v wsg={1} -v point_table=barriers_$BARRIERTYPE" ::: $WSGS 
 done
-# create user habitat endpoints and break streams at these locations as well
+
+# from habitat model, create user habitat endpoints and break streams at these locations as well
 $PSQL -f sql/user_habitat_classification_endpoints.sql
 parallel --jobs 4 --no-run-if-empty \
 		"echo \"SELECT bcfishpass.break_streams(:'point_table', :'wsg');\" | \
@@ -42,7 +49,7 @@ parallel --jobs 4 --no-run-if-empty \
 # INDEX 
 # -----
 # create tables holding lists of features that are downstream of individual stream segments
-for BARRIERTYPE in anthropogenic pscis dams dams_hydro ch_co_sk ch_co_sk_b st wct 
+for BARRIERTYPE in anthropogenic pscis dams dams_hydro $MODELS
 do
 	$PSQL -c "drop table if exists bcfishpass.barriers_"$BARRIERTYPE"_dnstr"
 	$PSQL -c "create table bcfishpass.barriers_"$BARRIERTYPE"_dnstr (segmented_stream_id text primary key, barriers_"$BARRIERTYPE"_dnstr text[])"
