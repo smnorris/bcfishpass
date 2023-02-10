a3bb551bc36bee844aec6d4e86df6b1f31ffe998
Simon Norris <snorris@hillcrestgeo.ca>
2023-02-09 20:45:46 -0800

    remove cw/q joins from stream load

--- model/access/sql/streams_load.sql
+++ model/access/sql/streams_load.sql
@@ -21,9 +21,6 @@ INSERT INTO bcfishpass.streams
   upstream_area_ha,
   stream_order_parent,
   stream_order_max,
-  --map_upstream,
-  --channel_width,
-  --mad_m3s,
   geom)
 SELECT
   s.linear_feature_id,
@@ -41,19 +38,12 @@ SELECT
   ua.upstream_area_ha,
   s.stream_order_parent,
   s.stream_order_max,
-  --p.map_upstream,
-  --cw.channel_width,
-  --COALESCE(mad_fndry.mad_m3s, mad.mad_m3s) as mad_m3s,  -- default to foundry data where available
   s.geom
 FROM whse_basemapping.fwa_stream_networks_sp s
 LEFT OUTER JOIN whse_basemapping.fwa_streams_watersheds_lut l
 ON s.linear_feature_id = l.linear_feature_id
 INNER JOIN whse_basemapping.fwa_watersheds_upstream_area ua
 ON l.watershed_feature_id = ua.watershed_feature_id
---LEFT OUTER JOIN bcfishpass.mean_annual_precip p ON s.wscode_ltree = p.wscode_ltree AND s.localcode_ltree = p.localcode_ltree
---LEFT OUTER JOIN bcfishpass.discharge mad ON s.linear_feature_id = mad.linear_feature_id
---LEFT OUTER JOIN foundry.fwa_streams_mad mad_fndry ON s.linear_feature_id = mad_fndry.linear_feature_id
---LEFT OUTER JOIN bcfishpass.channel_width cw ON s.linear_feature_id = cw.linear_feature_id
 WHERE
   s.watershed_group_code = :'wsg'
   AND s.wscode_ltree <@ '999' IS FALSE
