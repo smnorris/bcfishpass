223e06cfe736e72f9f7b6513c9542566e8315461
Simon Norris <snorris@hillcrestgeo.ca>
2023-03-06 16:51:53 -0800

    fix #286

--- model/access/sql/observations.sql
+++ model/access/sql/observations.sql
@@ -136,7 +136,9 @@ create index if not exists obsrvtn_geom_idx on bcfishpass.observations using gis
 -- create view of distinct observations for simpler species based queries
 drop materialized view if exists bcfishpass.observations_vw cascade;
 create materialized view bcfishpass.observations_vw as
-select
+
+with unnested as
+(select
     unnest(observation_ids) as fish_observation_point_id,
     unnest(species_codes) as species_code,
     unnest(observation_dates) as observation_date,
@@ -148,7 +150,27 @@ select
     downstream_route_measure,
     watershed_group_code,
     geom
-from bcfishpass.observations;
+from bcfishpass.observations)
+select 
+  u.fish_observation_point_id,
+  u.fish_obsrvtn_event_id,
+  u.linear_feature_id,
+  u.blue_line_key,
+  u.wscode_ltree,
+  u.localcode_ltree,
+  u.downstream_route_measure,
+  u.watershed_group_code,
+  u.species_code,
+  u.observation_date,
+  o.activity_code,
+  o.activity,
+  o.life_stage_code,
+  o.life_stage,
+  o.acat_report_url,
+  u.geom
+from unnested u
+inner join whse_fish.fiss_fish_obsrvtn_pnt_sp o
+on u.fish_observation_point_id = o.fish_observation_point_id;
 
 create unique index on bcfishpass.observations_vw (fish_observation_point_id);
 create index on bcfishpass.observations_vw using gist (wscode_ltree);
--- release/freshwater_fish_habitat_accessibility_model.sh
+++ release/freshwater_fish_habitat_accessibility_model.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
+# publish data to s3 
+# https://bcfishpass.s3.us-west-2.amazonaws.com/freshwater_fish_habitat_accessibility_MODEL.gpkg.zip
+
 # refresh the views
-#psql $DATABASE_URL -v ON_ERROR_STOP=1 -f sql/freshwater_fish_habitat_accessibility_model.sql
+psql $DATABASE_URL -v ON_ERROR_STOP=1 -f sql/freshwater_fish_habitat_accessibility_model.sql
 
 rm -rf freshwater_fish_habitat_accessibility_MODEL.gpkg*
 
@@ -14,6 +17,85 @@ ogr2ogr \
     -nln crossings \
     -nlt PointZM \
     -sql "select * from bcfishpass.freshwater_fish_habitat_accessibility_model_crossings_vw"
+
+
+echo 'dump gradient barriers table'
+ogr2ogr \
+    -f GPKG \
+    -append \
+    -update \
+    freshwater_fish_habitat_accessibility_MODEL.gpkg \
+    PG:$DATABASE_URL \
+    -nln gradient_barriers \
+    -nlt PointZM \
+    -sql "select 
+     ((((b.blue_line_key::bigint + 1) - 354087611) * 10000000) + round(b.downstream_route_measure::bigint))::text as gradient_barrier_id,
+    s.linear_feature_id,
+    b.blue_line_key,
+    b.downstream_route_measure,
+    s.wscode_ltree as wscode,
+    s.localcode_ltree as localcode,
+    b.watershed_group_code,
+    ST_Force2D((ST_Dump(ST_Locatealong(s.geom, b.downstream_route_measure))).geom)::geometry(Point,3005) as geom
+    from bcfishpass.gradient_barriers b
+    INNER JOIN whse_basemapping.fwa_stream_networks_sp s
+    ON b.blue_line_key = s.blue_line_key
+    AND s.downstream_route_measure <= b.downstream_route_measure
+    AND s.upstream_route_measure + .01 > b.downstream_route_measure
+    LEFT OUTER JOIN bcfishpass.user_barriers_definite_control p
+    ON b.blue_line_key = p.blue_line_key
+    AND abs(b.downstream_route_measure - p.downstream_route_measure) < 1
+    where gradient_class in (5, 10, 15, 20, 25, 30)
+    and (p.barrier_ind IS NULL or p.barrier_ind is true) -- do not include records forced to be passable in control table
+    limit 10"
+
+
+echo 'dumping subsurface flow barriers'
+ogr2ogr \
+    -f GPKG \
+    -append \
+    -update \
+    freshwater_fish_habitat_accessibility_MODEL.gpkg \
+    PG:$DATABASE_URL \
+    -nln barriers_subsurfaceflow \
+    -nlt PointZM \
+    -sql "select 
+     barriers_subsurfaceflow_id,
+     barrier_type,
+     barrier_name,
+     linear_feature_id,
+     blue_line_key,
+     watershed_key,
+     downstream_route_measure,
+     wscode_ltree as wscode,
+     localcode_ltree as localcode,
+     watershed_group_code,
+     geom
+     from bcfishpass.barriers_subsurfaceflow"
+
+
+echo 'dumping falls that are barriers'
+ogr2ogr \
+    -f GPKG \
+    -append \
+    -update \
+    freshwater_fish_habitat_accessibility_MODEL.gpkg \
+    PG:$DATABASE_URL \
+    -nln barriers_falls \
+    -nlt PointZM \
+    -sql "select 
+     barriers_falls_id,
+     barrier_type,
+     barrier_name,
+     linear_feature_id,
+     blue_line_key,
+     watershed_key,
+     downstream_route_measure,
+     wscode_ltree as wscode,
+     localcode_ltree as localcode,
+     watershed_group_code,
+     geom
+    from bcfishpass.barriers_falls"
 
 
 echo 'dumping barriers_salmon'
@@ -96,7 +178,24 @@ ogr2ogr \
     PG:$DATABASE_URL \
     -nln observations \
     -nlt PointZM \
-    -sql "select * from bcfishpass.freshwater_fish_habitat_accessibility_model_observations_vw"   
+    -sql "select  
+     fish_observation_point_id,
+     fish_obsrvtn_event_id,
+     linear_feature_id,
+     blue_line_key,
+     wscode_ltree as wscode,
+     localcode_ltree as localcode,
+     downstream_route_measure,
+     watershed_group_code,
+     species_code,
+     observation_date,
+     activity_code,
+     activity,
+     life_stage_code,
+     life_stage,
+     acat_report_url,
+     geom
+     from bcfishpass.freshwater_fish_habitat_accessibility_model_observations_vw"   
 
 echo 'dump to MODEL.gpkg complete'    
 
