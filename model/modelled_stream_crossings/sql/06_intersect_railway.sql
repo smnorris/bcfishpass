142baef0004d232d98ca1f203a62f0aeff6eb80d
Simon Norris <snorris@hillcrestgeo.ca>
2023-03-24 15:53:00 -0700

    fix #227

--- model/modelled_stream_crossings/sql/06_intersect_railway.sql
+++ model/modelled_stream_crossings/sql/06_intersect_railway.sql
@@ -15,7 +15,7 @@ roads AS
     railway_track_id,
     geom
   FROM whse_basemapping.gba_railway_tracks_sp r
-  WHERE r.track_classification != 'Ferry Route'
+  WHERE r.track_classification not in ('Ferry Route', 'Yard', 'Siding')
 ),
 
 -- overlay with streams, creating intersection points
