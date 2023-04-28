9a8bc4b4aa9ce658876a5aa7af8bc3363820e1ce
Simon Norris <snorris@hillcrestgeo.ca>
2023-04-27 19:30:52 -0700

    habitat view proof of concept

--- model/02_habitat_linear/sql/views/streams_ch_vw.sql
+++ model/02_habitat_linear/sql/views/streams_ch_vw.sql
@@ -1,89 +1,77 @@
 -- create per species views
 
-drop materialized view if exists bcfishpass.streams_ch_vw;
-
-create materialized view bcfishpass.streams_ch_vw as
-with status_codes as (
-select 
-  segmented_stream_id,
-  case 
-    -- migratory
-    when 
-      barriers_ch_cm_co_pk_sk_dnstr = array[]::text[] and
-      model_spawning_ch is null and
-      model_rearing_ch is null
-    then 'ACCESS' 
-    -- potential spawning
-    when model_spawning_ch is not null
-    then 'SPAWN'
-    -- potential rearing (and not spawning)
-    when
-      model_spawning_ch is null and
-      model_rearing_ch is not null
-    then 'REAR'
-  end as habitat_status,
-  case 
-    when remediated_dnstr is true then 'REMEDIATED'     -- this must be first condition as others below can also be true
-    when barriers_anthropogenic_dnstr is null then 'NONE'    -- no barriers dnstr
-    when barriers_anthropogenic_dnstr is not null and   -- modelled barrier dnstr
-      barriers_pscis_dnstr is null and
-      barriers_dams_dnstr is null and 
-      barriers_dams_hydro_dnstr is null then 'MODELLED'
-    when barriers_anthropogenic_dnstr is not null and   -- pscis barrier dnstr
-      barriers_pscis_dnstr is not null and
-      barriers_dams_dnstr is null and 
-      barriers_dams_hydro_dnstr is null then 'ASSESSED'
-    when barriers_anthropogenic_dnstr is not null and   -- dam barrier dnstr
-      barriers_dams_dnstr is not null and 
-      barriers_dams_hydro_dnstr is null then 'DAM'
-    when barriers_anthropogenic_dnstr is not null and   -- hydro dam barrier dnstr
-      barriers_dams_hydro_dnstr is not null then 'HYDRO'
-  end as anthropogenic_barrier_status,
-  case 
-    when feature_code = 'GA24850150' then 'INTERMITTENT' 
-    else NULL
-  end as intermittent_status
-from bcfishpass.streams 
-)
-
+create or replace view bcfishpass.streams_ch_vw as
 select
-  a.segmented_stream_id,
-  a.linear_feature_id,
-  a.edge_type,
-  a.blue_line_key,
-  a.watershed_key,
-  a.watershed_group_code,
-  a.downstream_route_measure,
-  a.length_metre,
-  a.waterbody_key,
-  a.wscode_ltree::text as wscode,
-  a.localcode_ltree::text as localcode,
-  a.gnis_name,
-  a.stream_order,
-  a.stream_magnitude,
-  a.gradient,
-  a.feature_code,
-  a.upstream_route_measure,
-  a.upstream_area_ha,
-  cw.channel_width,
-  --mad.mad_m3s,
-  a.stream_order_parent,
-  a.stream_order_max,
-  array_to_string(a.barriers_ch_cm_co_pk_sk_dnstr, ';') as barriers_ch_cm_co_pk_sk_dnstr,
-  array_to_string(a.barriers_anthropogenic_dnstr, ';') as barriers_anthropogenic_dnstr,
-  array_to_string(a.barriers_pscis_dnstr, ';') as barriers_pscis_dnstr,
-  array_to_string(a.barriers_dams_dnstr, ';') as barriers_dams_dnstr,
-  array_to_string(a.barriers_dams_hydro_dnstr, ';') as barriers_dams_hydro_dnstr,
-  array_to_string(a.crossings_dnstr, ';') as crossings_dnstr,
-  array_to_string(a.obsrvtn_event_upstr, ';') as obsrvtn_event_upstr,
-  array_to_string(a.obsrvtn_species_codes_upstr, ';') as obsrvtn_species_codes_upstr,
-  a.remediated_dnstr,
-  a.model_spawning_ch,
-  a.model_rearing_ch,
-  array_to_string(array[b.habitat_status, b.anthropogenic_barrier_status, b.intermittent_status], ';') as map_symbol_code,
-  a.geom
-from bcfishpass.streams a
-inner join status_codes b on a.segmented_stream_id = b.segmented_stream_id
-left outer join bcfishpass.discharge mad on a.linear_feature_id = mad.linear_feature_id
-left outer join bcfishpass.channel_width cw on a.linear_feature_id = cw.linear_feature_id
-where barriers_ch_cm_co_pk_sk_dnstr = array[]::text[];
+  s.segmented_stream_id,
+  s.linear_feature_id,
+  s.edge_type,
+  s.blue_line_key,
+  s.watershed_key,
+  s.watershed_group_code,
+  s.downstream_route_measure,
+  s.length_metre,
+  s.waterbody_key,
+  s.wscode_ltree::text as wscode,
+  s.localcode_ltree::text as localcode,
+  s.gnis_name,
+  s.stream_order,
+  s.stream_magnitude,
+  s.gradient,
+  s.feature_code,
+  s.upstream_route_measure,
+  s.upstream_area_ha,
+  s.map_upstream,
+  s.channel_width,
+  s.mad_m3s,
+  s.stream_order_parent,
+  s.stream_order_max,
+  array_to_string(s.barriers_ch_cm_co_pk_sk_dnstr, ';') as barriers_ch_cm_co_pk_sk_dnstr,
+  array_to_string(s.barriers_anthropogenic_dnstr, ';') as barriers_anthropogenic_dnstr,
+  array_to_string(s.barriers_pscis_dnstr, ';') as barriers_pscis_dnstr,
+  array_to_string(s.barriers_dams_dnstr, ';') as barriers_dams_dnstr,
+  array_to_string(s.barriers_dams_hydro_dnstr, ';') as barriers_dams_hydro_dnstr,
+  array_to_string(s.crossings_dnstr, ';') as crossings_dnstr,
+  array_to_string(s.obsrvtn_event_upstr, ';') as obsrvtn_event_upstr,
+  array_to_string(s.obsrvtn_species_codes_upstr, ';') as obsrvtn_species_codes_upstr,
+  s.remediated_dnstr,
+  h.spawning,
+  h.rearing,
+  array_to_string(array[
+  case
+    when
+      s.barriers_ch_cm_co_pk_sk_dnstr = array[]::text[] and
+      h.spawning is null and
+      h.rearing is null
+    then 'ACCESS'
+    when h.spawning is not null
+    then 'SPAWN'
+    when
+      h.spawning is null and
+      h.rearing is not null
+    then 'REAR'
+  end,
+  case
+    when s.remediated_dnstr is true then 'REMEDIATED'     -- this must be first condition as others below can also be true
+    when s.barriers_anthropogenic_dnstr is null then 'NONE'    -- no barriers dnstr
+    when s.barriers_anthropogenic_dnstr is not null and   -- modelled barrier dnstr
+      s.barriers_pscis_dnstr is null and
+      s.barriers_dams_dnstr is null and
+      s.barriers_dams_hydro_dnstr is null then 'MODELLED'
+    when s.barriers_anthropogenic_dnstr is not null and   -- pscis barrier dnstr
+      s.barriers_pscis_dnstr is not null and
+      s.barriers_dams_dnstr is null and
+      s.barriers_dams_hydro_dnstr is null then 'ASSESSED'
+    when s.barriers_anthropogenic_dnstr is not null and   -- dam barrier dnstr
+      s.barriers_dams_dnstr is not null and
+      s.barriers_dams_hydro_dnstr is null then 'DAM'
+    when s.barriers_anthropogenic_dnstr is not null and   -- hydro dam barrier dnstr
+      s.barriers_dams_hydro_dnstr is not null then 'HYDRO'
+  end,
+  case
+    when s.feature_code = 'GA24850150' then 'INTERMITTENT'
+    else NULL
+  end], ';') as map_symbol_code,
+  s.geom
+from bcfishpass.streams s
+left outer join bcfishpass.model_habitat_ch h on s.segmented_stream_id = h.segmented_stream_id
+where s.barriers_ch_cm_co_pk_sk_dnstr = array[]::text[];
