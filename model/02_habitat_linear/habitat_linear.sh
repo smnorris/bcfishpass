7b91d53a266461a740b2fab50b522fe42d76f7ba
Simon Norris <snorris@hillcrestgeo.ca>
2023-06-02 15:43:28 -0700

    fix bash loops

--- model/02_habitat_linear/habitat_linear.sh
+++ model/02_habitat_linear/habitat_linear.sh
@@ -5,14 +5,13 @@ set -euxo pipefail
 PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 
 WSGS=$($PSQL -AXt -c "SELECT watershed_group_code FROM bcfishpass.parameters_habitat_method")
-MODELS=$(ls sql/habitat*.sql | sed -e "s/sql\/habitat_//" | sed -e "s/.sql//")
-
 # run all habitat queries per watershed group
-for SP in $MODELS
+for sql in sql/habitat*.sql
 do
-  $PSQL -c "drop table if exists bcfishpass.habitat_"$SP
-  $PSQL -c "create table bcfishpass.habitat_"$SP" (segmented_stream_id text primary key, spawning boolean, rearing boolean)"
-  parallel $PSQL -f sql/habitat_$SP.sql -v wsg={1} ::: $WSGS
+  sp=$(echo $sql | sed -e "s/sql\/habitat_//" | sed -e "s/.sql//")
+  $PSQL -c "drop table if exists bcfishpass.habitat_"$sp
+  $PSQL -c "create table bcfishpass.habitat_"$sp" (segmented_stream_id text primary key, spawning boolean, rearing boolean)"
+  parallel $PSQL -f sql/habitat_$sp.sql -v wsg={1} ::: $WSGS
 done
 
 # horsefly sockeye have their own model due to trans-watershed group spawning/rearing effects
@@ -57,14 +56,15 @@ $PSQL -c "alter table bcfishpass.streams_model_habitat rename to streams"
 $PSQL -c "VACUUM ANALYZE bcfishpass.streams"
 
 # drop the no longer needed habitat_ tables
-for SP in $MODELS
+for sql in sql/habitat*.sql
 do
-    $PSQL -c "drop table if exists bcfishpass."$SP
+  sp=$(echo $sql | sed -e "s/sql\/habitat_//" | sed -e "s/.sql//")
+  $PSQL -c 'drop table if exists bcfishpass.habitat_'$sp
 done
 
 
 ## create output views
-for VW in $(ls sql/views/streams_*_vw.sql)
+for vw in sql/views/streams_*_vw.sql
 do
-  $PSQL -f $VW
+  $PSQL -f $vw
 done
