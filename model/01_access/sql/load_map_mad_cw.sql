ddf345f59afa3b87db68f0fbfe1247013ae84434
Simon Norris <snorris@hillcrestgeo.ca>
2023-09-04 21:30:47 -0700

    can't drop cw/map/mad, streams_vw depends on them

--- model/01_access/sql/load_map_mad_cw.sql
+++ model/01_access/sql/load_map_mad_cw.sql
@@ -1,21 +1,21 @@
 -- load pre-computed precip/discharge/channel width from file
 
-drop table if exists bcfishpass.discharge;
+# drop table if exists bcfishpass.discharge;          # can't drop this, streams_vw requires discharge
 
-create table bcfishpass.discharge
+create table if not exists bcfishpass.discharge
 (
  linear_feature_id    bigint          primary key,
  watershed_group_code text            ,
  mad_mm               double precision,
  mad_m3s              double precision
 );
-
+truncate bcfishpass.discharge;
 \copy bcfishpass.discharge FROM data/discharge.csv delimiter ',' csv header;
 
 
-drop table if exists bcfishpass.mean_annual_precip;
+drop table if exists bcfishpass.mean_annual_precip;  # as above, can't drop this
 
-create table bcfishpass.mean_annual_precip (
+create table if not exists bcfishpass.mean_annual_precip (
  id                    integer primary key,
  wscode_ltree          ltree  ,
  localcode_ltree       ltree  ,
@@ -24,7 +24,7 @@ create table bcfishpass.mean_annual_precip (
  map                   integer,
  map_upstream          integer 
 );
-
+truncate bcfishpass.mean_annual_precip;
 create index on bcfishpass.mean_annual_precip using gist (localcode_ltree);
 create index on bcfishpass.mean_annual_precip using btree (localcode_ltree);
 create index on bcfishpass.mean_annual_precip using gist (wscode_ltree);
@@ -33,12 +33,12 @@ create index on bcfishpass.mean_annual_precip using btree (wscode_ltree);
 \copy bcfishpass.mean_annual_precip FROM data/mean_annual_precip.csv delimiter ',' csv header;
 
 
-drop table if exists bcfishpass.channel_width;
+# drop table if exists bcfishpass.channel_width;     # can't drop this
 
-create table bcfishpass.channel_width (
+create table if not exists bcfishpass.channel_width (
  linear_feature_id     bigint           primary key,
  channel_width_source  text             ,
  channel_width         double precision 
 );
-
+truncate bcfishpass.channel_width;
 \copy bcfishpass.channel_width FROM data/channel_width.csv delimiter ',' csv header;
