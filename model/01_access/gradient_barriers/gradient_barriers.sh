9221e5e89b3acfb237829ffdb9517f6b39a08d34
Simon Norris <snorris@hillcrestgeo.ca>
2023-11-06 17:14:13 -0800

    do not load gradient barriers from file

--- model/01_access/gradient_barriers/gradient_barriers.sh
+++ model/01_access/gradient_barriers/gradient_barriers.sh
@@ -7,9 +7,5 @@ WSGS=$($PSQL -AXt -c "SELECT watershed_group_code FROM whse_basemapping.fwa_wate
 # create table
 $PSQL -f sql/gradient_barriers.sql
 
-# load from dumped csv
-$PSQL -c "\copy bcfishpass.gradient_barriers FROM PROGRAM 'curl https://bcfishpass.s3.us-west-2.amazonaws.com/gradient_barriers.csv.gz | \
-  gunzip' delimiter ',' csv header"
-
-# alternatively, re-process
-#parallel $PSQL -f sql/gradient_barriers_load.sql -v wsg={1} ::: $WSGS
+# load
+parallel $PSQL -f sql/gradient_barriers_load.sql -v wsg={1} ::: $WSGS
