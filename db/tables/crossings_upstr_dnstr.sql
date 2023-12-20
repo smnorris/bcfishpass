5d669f584c03651d17f6c3f12cb4ca80773b2c0b
Simon Norris <snorris@hillcrestgeo.ca>
2023-12-19 18:13:17 -0800

    add missing file

---
+++ db/tables/crossings_upstr_dnstr.sql
@@ -0,0 +1,25 @@
+-- note other crossings/barriers upstream / downstream of crossings in crossings table / barriers anth table
+
+drop table if exists bcfishpass.crossings_dnstr_crossings;
+create table bcfishpass.crossings_dnstr_crossings (
+	aggregated_crossings_id text primary key, 
+	features_dnstr text[]
+);
+
+drop table if exists bcfishpass.crossings_dnstr_barriers_anthropogenic;
+create table bcfishpass.crossings_dnstr_barriers_anthropogenic (
+	aggregated_crossings_id text primary key, 
+	features_dnstr text[]
+);
+
+drop table if exists bcfishpass.crossings_upstr_barriers_anthropogenic;
+create table bcfishpass.crossings_upstr_barriers_anthropogenic (
+	aggregated_crossings_id text primary key, 
+	features_upstr text[]
+	);
+
+drop table if exists bcfishpass.barriers_anthropogenic_dnstr_barriers_anthropogenic;
+create table bcfishpass.barriers_anthropogenic_dnstr_barriers_anthropogenic (
+	barriers_anthropogenic_id text primary key, 
+	features_dnstr text[]
+);
