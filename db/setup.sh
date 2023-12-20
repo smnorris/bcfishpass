1e04dc3f5005921cdb4fba1caa84f89ce6e22de2
Simon Norris <snorris@hillcrestgeo.ca>
2023-12-19 18:12:44 -0800

    create crossings_vw successfully

--- db/setup.sh
+++ db/setup.sh
@@ -4,18 +4,18 @@ set -euxo pipefail
 PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 
 # load schemas, tables functions
-for sql in sql/schemas/*.sql ; do
+for sql in schemas/*.sql ; do
 	$PSQL -f "$sql"
 done
 
-for sql in sql/tables/*.sql ; do
+for sql in tables/*.sql ; do
 	$PSQL -f "$sql"
 done
 
-for sql in sql/functions/*.sql ; do
+for sql in functions/*.sql ; do
 	$PSQL -f "$sql"
 done
 
-for sql in sql/views/*.sql ; do
+for sql in views/*.sql ; do
 	$PSQL -f "$sql"
 done
