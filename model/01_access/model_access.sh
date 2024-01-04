81445c041d4d49bcd9e6420650bae2dfbad4a15e
Simon Norris <snorris@hillcrestgeo.ca>
2024-01-04 09:00:15 -0800

    materialize access view

--- model/01_access/model_access.sh
+++ model/01_access/model_access.sh
@@ -101,6 +101,9 @@ $PARALLEL $PSQL -f sql/load_streams_upstr_observations.sql -v wsg={1} ::: $WSGS
 $PSQL -c "truncate bcfishpass.streams_dnstr_species"
 $PARALLEL $PSQL -f sql/load_streams_dnstr_species.sql -v wsg={1} ::: $WSGS
 
+# refresh materialized view
+$PSQL -c "refresh materialized view bcfishpass.streams_access_vw"
+
 # generate crossings access report
 $PSQL -c "truncate bcfishpass.crossings_upstream_access"
 parallel --halt now,fail=1 --jobs 2 --no-run-if-empty $PSQL -f sql/load_crossings_upstream_access_01.sql -v wsg={1} ::: $WSGS
