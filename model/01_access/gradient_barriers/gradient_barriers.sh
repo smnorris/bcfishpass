53c5bb74af327d0c1cfbd8c4c90db8d1a7595697
Simon Norris <snorris@hillcrestgeo.ca>
2024-02-21 11:51:27 -0800

    create empty gradient barrier table on db setup, tidy gradient barrier README

--- model/01_access/gradient_barriers/gradient_barriers.sh
+++ model/01_access/gradient_barriers/gradient_barriers.sh
@@ -4,8 +4,5 @@ set -euxo pipefail
 PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 WSGS=$($PSQL -AXt -c "SELECT watershed_group_code FROM bcfishpass.parameters_habitat_method")
 
-# create table
-$PSQL -f sql/gradient_barriers.sql
-
 # load
 parallel $PSQL -f sql/gradient_barriers_load.sql -v wsg={1} ::: $WSGS
