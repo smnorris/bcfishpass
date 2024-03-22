ddb7c09283bcb66a20fc27f4e793849a38bba09e
Simon Norris <snorris@hillcrestgeo.ca>
2024-03-22 15:21:50 -0700

    fix #483

--- model/01_access/model_access_1.sh
+++ model/01_access/model_access_1.sh
@@ -93,18 +93,3 @@ $PARALLEL \
     'true',
     :'wsg');\" | \
     $PSQL -v wsg={1}" ::: $WSGS
-
-# record observations downstream
-# (for convenience for field investigation and reporting, not as input into the individual models)
-$PSQL -c "truncate bcfishpass.streams_upstr_observations"
-$PARALLEL $PSQL -f sql/load_streams_upstr_observations.sql -v wsg={1} ::: $WSGS
-$PSQL -c "truncate bcfishpass.streams_dnstr_species"
-$PARALLEL $PSQL -f sql/load_streams_dnstr_species.sql -v wsg={1} ::: $WSGS
-
-# refresh materialized view
-$PSQL -c "refresh materialized view bcfishpass.streams_access_vw"
-
-# generate crossings access report
-$PSQL -c "truncate bcfishpass.crossings_upstream_access"
-parallel --halt now,fail=1 --jobs 2 --no-run-if-empty $PSQL -f sql/load_crossings_upstream_access_01.sql -v wsg={1} ::: $WSGS
-parallel --halt now,fail=1 --jobs 2 --no-run-if-empty $PSQL -f sql/load_crossings_upstream_access_02.sql -v wsg={1} ::: $WSGS
