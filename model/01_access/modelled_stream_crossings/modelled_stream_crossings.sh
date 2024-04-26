44dab9f5c3714b216b216729e82d5fb5b6b7906a
Simon Norris <snorris@hillcrestgeo.ca>
2024-04-25 17:54:36 -0700

    load modelled crossings from temp table on completion

--- model/01_access/modelled_stream_crossings/modelled_stream_crossings.sh
+++ model/01_access/modelled_stream_crossings/modelled_stream_crossings.sh
@@ -28,4 +28,43 @@ $PSQL -f sql/08_identify_open_bottom_structures.sql
 # assign modelled_crossing_id from previous version to ensure consistency
 $PSQL -f sql/09_match_existing_crossings.sql
 
-#$PSQL -c "DROP TABLE bcfishpass.modelled_stream_crossings_build"
+# load new data to primary table
+$PSQL -c "truncate bcfishpass.modelled_stream_crossings;
+INSERT INTO bcfishpass.modelled_stream_crossings (
+  modelled_crossing_id,
+  modelled_crossing_type,
+  modelled_crossing_type_source,
+  transport_line_id,
+  ften_road_section_lines_id,
+  og_road_segment_permit_id,
+  og_petrlm_dev_rd_pre06_pub_id,
+  railway_track_id,
+  linear_feature_id,
+  blue_line_key,
+  downstream_route_measure,
+  wscode_ltree,
+  localcode_ltree,
+  watershed_group_code,
+  geom
+)
+select
+  modelled_crossing_id,
+  modelled_crossing_type,
+  modelled_crossing_type_source,
+  transport_line_id,
+  ften_road_section_lines_id,
+  og_road_segment_permit_id,
+  og_petrlm_dev_rd_pre06_pub_id,
+  railway_track_id,
+  linear_feature_id,
+  blue_line_key,
+  downstream_route_measure,
+  wscode_ltree,
+  localcode_ltree,
+  watershed_group_code,
+  geom
+from bcfishpass.modelled_stream_crossings_output"
+
+# cleanup
+$PSQL -c "DROP TABLE bcfishpass.modelled_stream_crossings_build"
+$PSQL -c "DROP TABLE bcfishpass.modelled_stream_crossings_output"
