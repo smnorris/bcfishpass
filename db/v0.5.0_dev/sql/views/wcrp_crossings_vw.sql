b8b31024159537564258e7ba2bb252387d517bd2
Simon Norris <snorris@hillcrestgeo.ca>
2024-07-03 17:55:15 -0700

    update wcrp crossings view to report on upstream barriers having spawn/rear habitat for wcrp target spp upstream, fixes #527

--- db/v0.5.0_dev/sql/views/wcrp_crossings_vw.sql
+++ db/v0.5.0_dev/sql/views/wcrp_crossings_vw.sql
@@ -1,5 +1,29 @@
 -- wcrp version of the output crossings view -
 create materialized view bcfishpass.crossings_wcrp_vw as
+
+-- find upstream crossings with wcrp 'all spawning rearing habitat' upstream
+with upstr_wcrp_barriers as materialized (
+  select distinct
+   ba.aggregated_crossings_id,
+   h.aggregated_crossings_id as upstr_barriers,
+   h.all_spawningrearing_km
+  from bcfishpass.crossings_upstr_barriers_anthropogenic ba
+  inner join bcfishpass.crossings_upstream_habitat_wcrp h on h.aggregated_crossings_id = any(ba.features_upstr)
+  where h.all_spawningrearing_km > 0
+  order by ba.aggregated_crossings_id, h.aggregated_crossings_id
+),
+
+-- aggregate the upstream wcrp crossings into a list and count
+upstr_wcrp_barriers_list as (
+  select
+    aggregated_crossings_id,
+    array_to_string(array_agg(upstr_barriers), ';') as barriers_anthropogenic_habitat_wcrp_upstr,
+    coalesce(array_length(array_agg(upstr_barriers), 1), 0) as barriers_anthropogenic_habitat_wcrp_upstr_count
+  from upstr_wcrp_barriers
+  group by aggregated_crossings_id
+  order by aggregated_crossings_id
+)
+
 select
   -- joining to streams based on measure can be error prone due to precision.
   -- Join to streams on linear_feature_id and keep the first result
@@ -32,8 +56,8 @@ select
   c.gnis_stream_name,
   array_to_string(ad.features_dnstr, ';') as barriers_anthropogenic_dnstr,
   coalesce(array_length(ad.features_dnstr, 1), 0) as barriers_anthropogenic_dnstr_count,
-  array_to_string(aum.barriers_upstr_ch_cm_co_pk_sk, ';') as barriers_anthropogenic_ch_cm_co_pk_sk_upstr,
-  coalesce(array_length(aum.barriers_upstr_ch_cm_co_pk_sk, 1), 0) as barriers_anthropogenic_ch_cm_co_pk_sk_upstr_count,
+  uwbl.barriers_anthropogenic_habitat_wcrp_upstr,
+  uwbl.barriers_anthropogenic_habitat_wcrp_upstr_count,
 
   -- habitat models
   h.ch_spawning_km,
@@ -89,7 +113,7 @@ left outer join bcfishpass.crossings_upstr_observations_vw cuo on c.aggregated_c
 left outer join bcfishpass.crossings_dnstr_crossings cd on c.aggregated_crossings_id = cd.aggregated_crossings_id
 left outer join bcfishpass.crossings_dnstr_barriers_anthropogenic ad on c.aggregated_crossings_id = ad.aggregated_crossings_id
 left outer join bcfishpass.crossings_upstr_barriers_anthropogenic au on c.aggregated_crossings_id = au.aggregated_crossings_id
-left outer join bcfishpass.crossings_upstr_barriers_per_model_vw aum on c.aggregated_crossings_id = aum.aggregated_crossings_id
+left outer join upstr_wcrp_barriers_list uwbl on c.aggregated_crossings_id = uwbl.aggregated_crossings_id
 left outer join bcfishpass.crossings_upstream_access a on c.aggregated_crossings_id = a.aggregated_crossings_id
 left outer join bcfishpass.crossings_upstream_habitat h on c.aggregated_crossings_id = h.aggregated_crossings_id
 left outer join bcfishpass.crossings_upstream_habitat_wcrp h_wcrp on c.aggregated_crossings_id = h_wcrp.aggregated_crossings_id
@@ -97,33 +121,3 @@ left outer join bcfishpass.streams s on c.linear_feature_id = s.linear_feature_i
 left outer join whse_basemapping.dbm_mof_50k_grid t ON ST_Intersects(c.geom, t.geom)
 left outer join bcfishpass.wcrp_ranked_barriers r ON c.aggregated_crossings_id = r.aggregated_crossings_id
 order by c.aggregated_crossings_id, s.downstream_route_measure;
-
-create unique index on bcfishpass.crossings_wcrp_vw (aggregated_crossings_id);
-create index on bcfishpass.crossings_wcrp_vw using gist (geom);
-
-
-comment on column bcfishpass.crossings_wcrp_vw.aggregated_crossings_id IS 'unique identifier for crossing, generated from stream_crossing_id, modelled_crossing_id + 1000000000, user_barrier_anthropogenic_id + 1200000000, cabd_id';
-comment on column bcfishpass.crossings_wcrp_vw.modelled_crossing_id IS 'Modelled crossing unique identifier';
-comment on column bcfishpass.crossings_wcrp_vw.crossing_source IS 'Data source for the crossing, one of: {PSCIS,MODELLED CROSSINGS,CABD,MISC BARRIERS}';
-comment on column bcfishpass.crossings_wcrp_vw.pscis_status IS 'From PSCIS, the current_pscis_status of the crossing, one of: {ASSESSED,HABITAT CONFIRMATION,DESIGN,REMEDIATED}';
-comment on column bcfishpass.crossings_wcrp_vw.crossing_type_code IS 'Defines the type of crossing present at the location of the stream crossing. Acceptable types are: OBS = Open Bottom Structure CBS = Closed Bottom Structure OTHER = Crossing structure does not fit into the above categories. Eg: ford, wier';
-comment on column bcfishpass.crossings_wcrp_vw.crossing_subtype_code IS 'Further definition of the type of crossing, one of {BRIDGE,CRTBOX,DAM,FORD,OVAL,PIPEARCH,ROUND,WEIR,WOODBOX,NULL}';
-comment on column bcfishpass.crossings_wcrp_vw.barrier_status IS 'The evaluation of the crossing as a barrier to the fish passage. From PSCIS, this is based on the FINAL SCORE value. For other data sources this varies. Acceptable Values are: PASSABLE - Passable, POTENTIAL - Potential or partial barrier, BARRIER - Barrier, UNKNOWN - Other';
-comment on column bcfishpass.crossings_wcrp_vw.pscis_road_name  IS 'PSCIS road name, taken from the PSCIS assessment data submission';
-comment on column bcfishpass.crossings_wcrp_vw.pscis_stream_name  IS 'PSCIS stream name, taken from the PSCIS assessment data submission';
-comment on column bcfishpass.crossings_wcrp_vw.pscis_assessment_comment  IS 'PSCIS assessment_comment, taken from the PSCIS assessment data submission';
-comment on column bcfishpass.crossings_wcrp_vw.pscis_assessment_date  IS 'PSCIS assessment_date, taken from the PSCIS assessment data submission';
-comment on column bcfishpass.crossings_wcrp_vw.transport_line_structured_name_1 IS 'DRA road name, taken from the nearest DRA road (within 30m)';
-comment on column bcfishpass.crossings_wcrp_vw.rail_track_name IS 'Railway name, taken from nearest railway (within 25m)';
-comment on column bcfishpass.crossings_wcrp_vw.dam_name IS 'See CABD dams column: dam_name_en';
-comment on column bcfishpass.crossings_wcrp_vw.dam_height IS 'See CABD dams column: dam_height';
-comment on column bcfishpass.crossings_wcrp_vw.dam_owner IS 'See CABD dams column: owner';
-comment on column bcfishpass.crossings_wcrp_vw.dam_use IS 'See CABD table dam_use_codes';
-comment on column bcfishpass.crossings_wcrp_vw.dam_operating_status IS 'See CABD dams column dam_operating_status';
-comment on column bcfishpass.crossings_wcrp_vw.utm_zone IS 'UTM ZONE is a segment of the Earths surface 6 degrees of longitude in width. The zones are numbered eastward starting at the meridian 180 degrees from the prime meridian at Greenwich. There are five zones numbered 7 through 11 that cover British Columbia, e.g., Zone 10 with a central meridian at -123 degrees.';
-comment on column bcfishpass.crossings_wcrp_vw.utm_easting IS 'UTM EASTING is the distance in meters eastward to or from the central meridian of a UTM zone with a false easting of 500000 meters. e.g., 440698';
-comment on column bcfishpass.crossings_wcrp_vw.utm_northing IS 'UTM NORTHING is the distance in meters northward from the equator. e.g., 6197826';
-comment on column bcfishpass.crossings_wcrp_vw.blue_line_key IS 'From BC FWA, uniquely identifies a single flow line such that a main channel and a secondary channel with the same watershed code would have different blue line keys (the Fraser River and all side channels have different blue line keys).';
-comment on column bcfishpass.crossings_wcrp_vw.watershed_group_code IS 'The watershed group code associated with the feature.';
-comment on column bcfishpass.crossings_wcrp_vw.gnis_stream_name IS 'The BCGNIS (BC Geographical Names Information System) name associated with the FWA stream';
-comment on column bcfishpass.crossings_wcrp_vw.geom IS 'The point geometry associated with the feature';
