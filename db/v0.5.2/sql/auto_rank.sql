03d8a41e086b83c7bf7ecb32351ad88814dcf55b
andrewp-CWF <146005147+andrewp-CWF@users.noreply.github.com>
2024-08-13 15:38:42 -0400

    Ranking update (#562)
    * Added permission assignment to auto_rank.py
    
    * Update auto_rank.py with truncate instead of drop
    
    * Created auto_rank.sql
    
    This file includes a function to create the ranked_barrier table for a given wcrp in the given schema
    
    * merge, fix typo
    
    ---------
    
    Co-authored-by: Simon Norris <snorris@hillcrestgeo.ca>

---
+++ db/v0.5.2/sql/auto_rank.sql
@@ -0,0 +1,51 @@
+-- create table for ranked list
+
+CREATE FUNCTION bcfishpass.create_rank_table(schema_name text, wcrp text)
+    RETURNS VOID
+    LANGUAGE plpgsql AS
+$func$
+BEGIN
+
+    EXECUTE format('
+        CREATE TABLE IF NOT EXISTS %I.%I
+        (
+            aggregated_crossings_id text,
+            crossing_source text,
+            crossing_feature_type text,
+            pscis_status text,
+            crossing_type_code text,
+            barrier_status text,
+            pscis_road_name text,
+            pscis_assessment_comment text,
+            pscis_assessment_date date,
+            utm_zone integer,
+            utm_easting integer,
+            utm_northing integer,
+            blue_line_key integer,
+            watershed_group_code integer,
+            gnis_stream_name text,
+            barriers_anthropogenic_dnstr text,
+            barriers_anthropogenic_habitat_wcrp_upstr text,
+            barriers_anthropogenic_habitat_wcrp_upstr_count integer,
+            all_spawning_km double precision,
+            all_spawning_belowupstrbarriers_km double precision,
+            all_rearing_km double precision,
+            all_rearing_belowupstrbarriers_km double precision,
+            all_spawningrearing_km double precision,
+            all_spawningrearing_belowupstrbarriers_km double precision,
+            set_id numeric,
+            total_hab_gain_set numeric,
+            num_barriers_set integer,
+            avg_gain_per_barrier numeric,
+            dnstr_set_ids character varying[],
+            rank_avg_gain_per_barrier numeric,
+            rank_avg_gain_tiered numeric,
+            tier_combined character varying,
+            geom geometry
+        )',
+        schema_name,
+        'ranked_barriers_' || wcrp
+    );
+
+END
+$func$
