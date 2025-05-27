67ff842053fb4fb77dde46ddc8f0396a692f8caa
Simon Norris <snorris@hillcrestgeo.ca>
2025-05-27 14:57:35 -0700

    use 0 for observed non habitat to avoid conflict with not modelled

--- db/v0.6.0/sql/01_modify_habitat_codes.sql
+++ db/v0.6.0/sql/01_modify_habitat_codes.sql
@@ -152,56 +152,56 @@ create materialized view bcfishpass.streams_habitat_linear_vw as
 select
   s.segmented_stream_id,
   case
-    when u.spawning_bt is false then -1
+    when u.spawning_bt is false then 0
     when coalesce(bt.spawning, false) is true and coalesce(u.spawning_bt, false) is false then 1
     when coalesce(bt.spawning, false) is true and coalesce(u.spawning_bt, false) is true then 2
     when coalesce(bt.spawning, false) is false and coalesce(u.spawning_bt, false) is true then 3
     else 0
   end as spawning_bt,
   case
-    when u.spawning_ch is false then -1
+    when u.spawning_ch is false then 0
     when coalesce(ch.spawning, false) is true and coalesce(u.spawning_ch, false) is false then 1
     when coalesce(ch.spawning, false) is true and coalesce(u.spawning_ch, false) is true then 2
     when coalesce(ch.spawning, false) is false and coalesce(u.spawning_ch, false) is true then 3
     else 0
   end as spawning_ch,
   case
-    when u.spawning_cm is false then -1
+    when u.spawning_cm is false then 0
     when coalesce(cm.spawning, false) is true and coalesce(u.spawning_cm, false) is false then 1
     when coalesce(cm.spawning, false) is true and coalesce(u.spawning_cm, false) is true then 2
     when coalesce(cm.spawning, false) is false and coalesce(u.spawning_cm, false) is true then 3
     else 0
   end as spawning_cm,
   case
-    when u.spawning_co is false then -1
+    when u.spawning_co is false then 0
     when coalesce(co.spawning, false) is true and coalesce(u.spawning_co, false) is false then 1
     when coalesce(co.spawning, false) is true and coalesce(u.spawning_co, false) is true then 2
     when coalesce(co.spawning, false) is false and coalesce(u.spawning_co, false) is true then 3
     else 0
   end as spawning_co,
   case
-    when u.spawning_pk is false then -1
+    when u.spawning_pk is false then 0
     when coalesce(pk.spawning, false) is true and coalesce(u.spawning_pk, false) is false then 1
     when coalesce(pk.spawning, false) is true and coalesce(u.spawning_pk, false) is true then 2
     when coalesce(pk.spawning, false) is false and coalesce(u.spawning_pk, false) is true then 3
     else 0
   end as spawning_pk,
   case
-    when u.spawning_sk is false then -1
+    when u.spawning_sk is false then 0
     when coalesce(sk.spawning, false) is true and coalesce(u.spawning_sk, false) is false then 1
     when coalesce(sk.spawning, false) is true and coalesce(u.spawning_sk, false) is true then 2
     when coalesce(sk.spawning, false) is false and coalesce(u.spawning_sk, false) is true then 3
     else 0
   end as spawning_sk,
   case
-    when u.spawning_st is false then -1
+    when u.spawning_st is false then 0
     when coalesce(st.spawning, false) is true and coalesce(u.spawning_st, false) is false then 1
     when coalesce(st.spawning, false) is true and coalesce(u.spawning_st, false) is true then 2
     when coalesce(st.spawning, false) is false and coalesce(u.spawning_st, false) is true then 3
     else 0
   end as spawning_st,
   case
-    when u.spawning_wct is false then -1
+    when u.spawning_wct is false then 0
     when coalesce(wct.spawning, false) is true and coalesce(u.spawning_wct, false) is false then 1
     when coalesce(wct.spawning, false) is true and coalesce(u.spawning_wct, false) is true then 2
     when coalesce(wct.spawning, false) is false and coalesce(u.spawning_wct, false) is true then 3
@@ -209,42 +209,42 @@ select
   end as spawning_wct,
 
   case
-    when u.rearing_bt is false then -1
+    when u.rearing_bt is false then 0
     when coalesce(bt.rearing, false) is true and coalesce(u.rearing_bt, false) is false then 1
     when coalesce(bt.rearing, false) is true and coalesce(u.rearing_bt, false) is true then 2
     when coalesce(bt.rearing, false) is false and coalesce(u.rearing_bt, false) is true then 3
     else 0
   end as rearing_bt,
   case
-    when u.rearing_ch is false then -1
+    when u.rearing_ch is false then 0
     when coalesce(ch.rearing, false) is true and coalesce(u.rearing_ch, false) is false then 1
     when coalesce(ch.rearing, false) is true and coalesce(u.rearing_ch, false) is true then 2
     when coalesce(ch.rearing, false) is false and coalesce(u.rearing_ch, false) is true then 3
     else 0
   end as rearing_ch,
   case
-    when u.rearing_co is false then -1
+    when u.rearing_co is false then 0
     when coalesce(co.rearing, false) is true and coalesce(u.rearing_co, false) is false then 1
     when coalesce(co.rearing, false) is true and coalesce(u.rearing_co, false) is true then 2
     when coalesce(co.rearing, false) is false and coalesce(u.rearing_co, false) is true then 3
     else 0
   end as rearing_co,
   case
-    when u.rearing_sk is false then -1
+    when u.rearing_sk is false then 0
     when coalesce(sk.rearing, false) is true and coalesce(u.rearing_sk, false) is false then 1
     when coalesce(sk.rearing, false) is true and coalesce(u.rearing_sk, false) is true then 2
     when coalesce(sk.rearing, false) is false and coalesce(u.rearing_sk, false) is true then 3
     else 0
   end as rearing_sk,
   case
-    when u.rearing_st is false then -1
+    when u.rearing_st is false then 0
     when coalesce(st.rearing, false) is true and coalesce(u.rearing_st, false) is false then 1
     when coalesce(st.rearing, false) is true and coalesce(u.rearing_st, false) is true then 2
     when coalesce(st.rearing, false) is false and coalesce(u.rearing_st, false) is true then 3
     else 0
   end as rearing_st,
   case
-    when u.rearing_wct is false then -1
+    when u.rearing_wct is false then 0
     when coalesce(wct.rearing, false) is true and coalesce(u.rearing_wct, false) is false then 1
     when coalesce(wct.rearing, false) is true and coalesce(u.rearing_wct, false) is true then 2
     when coalesce(wct.rearing, false) is false and coalesce(u.rearing_wct, false) is true then 3
