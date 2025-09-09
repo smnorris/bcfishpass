c48a54e1a99f2f28b8a2981e3261a0d9c4628c46
Simon Norris <snorris@hillcrestgeo.ca>
2025-09-09 09:28:42 -0700

    wrap migration in transaction, create wcrp_hors schema

--- db/v0.6.0/sql/04_wcrp_initialize_rank_tables.sql
+++ db/v0.6.0/sql/04_wcrp_initialize_rank_tables.sql
@@ -1,22 +1,26 @@
-CREATE SCHEMA IF NOT EXISTS wcrp_bela_atna_necl;
-CREATE SCHEMA IF NOT EXISTS wcrp_bowr_ques_carr;
-CREATE SCHEMA IF NOT EXISTS wcrp_bulk;
-CREATE SCHEMA IF NOT EXISTS wcrp_elkr;
-CREATE SCHEMA IF NOT EXISTS wcrp_lnic;
-CREATE SCHEMA IF NOT EXISTS wcrp_morr;
-CREATE SCHEMA IF NOT EXISTS wcrp_tho_shu;
-CREATE SCHEMA IF NOT EXISTS wcrp_tuzistol_tah;
+begin;
 
+  CREATE SCHEMA IF NOT EXISTS wcrp_bela_atna_necl;
+  CREATE SCHEMA IF NOT EXISTS wcrp_bowr_ques_carr;
+  CREATE SCHEMA IF NOT EXISTS wcrp_bulk;
+  CREATE SCHEMA IF NOT EXISTS wcrp_elkr;
+  CREATE SCHEMA IF NOT EXISTS wcrp_hors;
+  CREATE SCHEMA IF NOT EXISTS wcrp_lnic;
+  CREATE SCHEMA IF NOT EXISTS wcrp_morr;
+  CREATE SCHEMA IF NOT EXISTS wcrp_tho_shu;
+  CREATE SCHEMA IF NOT EXISTS wcrp_tuzistol_tah;
 
-SELECT bcfishpass.create_rank_table('wcrp_bela_atna_necl','bela_atna_necl');
-SELECT bcfishpass.create_rank_table('wcrp_bowr_ques_carr','bowr_ques_carr');
-SELECT bcfishpass.create_rank_table('wcrp_bulk','bulk');
-SELECT bcfishpass.create_rank_table('wcrp_elkr','elkr_dnstr');
-SELECT bcfishpass.create_rank_table('wcrp_elkr','elkr_upstr');
-SELECT bcfishpass.create_rank_table('wcrp_hors','hors');
-SELECT bcfishpass.create_rank_table('wcrp_lnic','lnic');
-SELECT bcfishpass.create_rank_table('wcrp_morr','morr');
-SELECT bcfishpass.create_rank_table('wcrp_tho_shu','bonp');
-SELECT bcfishpass.create_rank_table('wcrp_tho_shu','bessette');
-SELECT bcfishpass.create_rank_table('wcrp_tho_shu','eagle');
-SELECT bcfishpass.create_rank_table('wcrp_tuzistol_tah','tuzistol_tah');
+  SELECT bcfishpass.create_rank_table('wcrp_bela_atna_necl','bela_atna_necl');
+  SELECT bcfishpass.create_rank_table('wcrp_bowr_ques_carr','bowr_ques_carr');
+  SELECT bcfishpass.create_rank_table('wcrp_bulk','bulk');
+  SELECT bcfishpass.create_rank_table('wcrp_elkr','elkr_dnstr');
+  SELECT bcfishpass.create_rank_table('wcrp_elkr','elkr_upstr');
+  SELECT bcfishpass.create_rank_table('wcrp_hors','hors');
+  SELECT bcfishpass.create_rank_table('wcrp_lnic','lnic');
+  SELECT bcfishpass.create_rank_table('wcrp_morr','morr');
+  SELECT bcfishpass.create_rank_table('wcrp_tho_shu','bonp');
+  SELECT bcfishpass.create_rank_table('wcrp_tho_shu','bessette');
+  SELECT bcfishpass.create_rank_table('wcrp_tho_shu','eagle');
+  SELECT bcfishpass.create_rank_table('wcrp_tuzistol_tah','tuzistol_tah');
+
+commit;
