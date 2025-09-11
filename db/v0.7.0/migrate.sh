94ca6dd21d5b84186a875ddfe378d387cf68fcf8
Simon Norris <snorris@hillcrestgeo.ca>
2025-09-11 13:00:03 -0700

    move rebuild of slow fptwg views to separate script

--- db/v0.7.0/migrate.sh
+++ db/v0.7.0/migrate.sh
@@ -5,4 +5,8 @@ PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 
 # see migrate.sql for notes
 $PSQL -f sql/migrate.sql
+
+# on fptwg dbs, recreate materialized views for reporting
+# $PSQL -f sql/fptwg.sql
+
 $PSQL -c "update bcfishpass.db_version set tag = '${PWD##*/}'"
