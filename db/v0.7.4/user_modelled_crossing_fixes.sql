5ea8ac788289e963a76685b2902602bcc7368cb2
Simon Norris <snorris@hillcrestgeo.ca>
2025-11-10 14:04:40 -0800

    make user_modelled_crossing_fixes unique

--- db/v0.7.4/user_modelled_crossing_fixes.sql
+++ db/v0.7.4/user_modelled_crossing_fixes.sql
@@ -2,9 +2,43 @@
 
 BEGIN;
 
+  CREATE TEMPORARY TABLE user_modelled_crossing_fixes_unique AS
+    SELECT * FROM (
+      SELECT DISTINCT ON (modelled_crossing_id)
+        modelled_crossing_id,
+        structure,
+        watershed_group_code,
+        reviewer_name,
+        review_date,
+        source,
+        notes
+      FROM bcfishpass.user_modelled_crossing_fixes
+      ORDER BY modelled_crossing_id, review_date
+      ) as f
+    ORDER BY review_date;
+
   TRUNCATE bcfishpass.user_modelled_crossing_fixes;
 
   ALTER TABLE bcfishpass.user_modelled_crossing_fixes
   ADD PRIMARY KEY (modelled_crossing_id);
+
+  INSERT INTO bcfishpass.user_modelled_crossing_fixes (
+    modelled_crossing_id,
+    structure,
+    watershed_group_code,
+    reviewer_name,
+    review_date,
+    source,
+    notes
+  )
+  SELECT
+    modelled_crossing_id,
+    structure,
+    watershed_group_code,
+    reviewer_name,
+    review_date,
+    source,
+    notes
+  FROM user_modelled_crossing_fixes_unique;
 
 COMMIT;
