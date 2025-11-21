c6f119a0a25756fca9bb4b0392dfa8ac2858a99d
Simon Norris <snorris@hillcrestgeo.ca>
2025-11-21 12:50:29 -0800

    clean up parallel incantations

--- model/01_access/01_model_access_natural.sh
+++ model/01_access/01_model_access_natural.sh
@@ -4,6 +4,12 @@ set -euxo pipefail
 PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 WSGS=$($PSQL -AtX -c "SELECT watershed_group_code FROM bcfishpass.parameters_habitat_method")
 
+# natural barriers are listed here
+# per species models are derived from names of sql/model_access_<spp>.sql files
+NATURAL_BARRIERS=("falls" "user_definite")
+MODELS=$(ls sql/model_access*.sql | sed -e "s/sql\/model_access_//" | sed -e "s/.sql//")
+
+
 # --
 # -- refresh observations and falls
 # -- 
@@ -14,7 +20,6 @@ $PSQL -f sql/load_falls.sql
 # -- collect natural barrier sources that tend to change into to bcfishpass.barriers_<source>
 # -- (reasonably constant FWA natural barriers are loaded separately)
 # --
-NATURAL_BARRIERS=("falls" "user_definite")
 for BARRIERTYPE in "${NATURAL_BARRIERS[@]}"; do
     
     # drop/create the table
@@ -27,8 +32,6 @@ done
 # --
 # -- then collect the source natural barriers in to per-species barrier tables, defining naturally accessible stream
 # --
-
-MODELS=$(ls sql/model_access*.sql | sed -e "s/sql\/model_access_//" | sed -e "s/.sql//")
 for SPP in $MODELS; do
   
   # drop/create the table
@@ -79,15 +82,14 @@ for SPP in $MODELS; do
 done
 
 
-# use max 4 concurrent jobs for subsequent steps
-PARALLEL="parallel --halt now,fail=1 --jobs 4 --no-run-if-empty"
-
 # -----
 # LOAD STREAMS
 # -----
 # clear streams table and load data from FWA
 $PSQL -c "truncate bcfishpass.streams"
-$PARALLEL $PSQL -f sql/load_streams.sql -v wsg={1} ::: $WSGS
+parallel --halt now,fail=1 --jobs 4 --no-run-if-empty \
+  $PSQL -f sql/load_streams.sql -v wsg={1} ::: $WSGS
+
 $PSQL -c "VACUUM ANALYZE bcfishpass.streams"
 
 
@@ -95,49 +97,52 @@ $PSQL -c "VACUUM ANALYZE bcfishpass.streams"
 # BREAK STREAMS
 # -----
 # break at observations
-$PARALLEL \
-  "echo \"SELECT bcfishpass.break_streams(:'point_table', :'wsg');\" | \
-  $PSQL -v wsg={1} -v point_table=observations" ::: $WSGS
+parallel --halt now,fail=1 --jobs 4 --no-run-if-empty "
+  $PSQL -c \"
+  SELECT bcfishpass.break_streams('observations', '{1}');\"
+" ::: $WSGS
 
 # break at natural barriers for all given species scenarios
-for BARRIERTYPE in $MODELS
-do
-  $PARALLEL \
-  "echo \"SELECT bcfishpass.break_streams(:'point_table', :'wsg');\" | \
-  $PSQL -v wsg={1} -v point_table=barriers_$BARRIERTYPE" ::: $WSGS
-done
+parallel --halt now,fail=1 --jobs 4 --no-run-if-empty "
+  $PSQL -c \"
+  SELECT bcfishpass.break_streams('barriers_{1}', '{2}');\"
+" ::: $MODELS ::: $WSGS
 
 # break streams at user habitat definition endpoints
 $PSQL -f sql/user_habitat_classification_endpoints.sql
-$PARALLEL \
-  "echo \"SELECT bcfishpass.break_streams(:'point_table', :'wsg');\" | \
-  $PSQL -v wsg={1} -v point_table=user_habitat_classification_endpoints" ::: $WSGS
+parallel --halt now,fail=1 --jobs 4 --no-run-if-empty "
+  $PSQL -c \"
+  SELECT bcfishpass.break_streams('user_habitat_classification_endpoints', '{1}');\"
+" ::: $WSGS
 
 
 # -----
 # INDEX 
 # -----
-# create tables holding lists of features that are downstream of individual stream segments
+# load table listing barriers that are downstream of individual stream segments
 $PSQL -c "truncate bcfishpass.streams_dnstr_barriers";
-for BARRIERTYPE in $MODELS
-do
-  $PARALLEL \
-  "echo \"SELECT bcfishpass.load_dnstr(
-      'bcfishpass.streams',
-      'segmented_stream_id',
-      'bcfishpass.barriers_\"$BARRIERTYPE\"',
-      'barriers_\"$BARRIERTYPE\"_id',
-      'bcfishpass.streams_dnstr_barriers',
-      'barriers_\"$BARRIERTYPE\"_dnstr',
-      'true',
-      :'wsg');\" | \
-  $PSQL -v wsg={1}" ::: $WSGS
-done
-
+LOGFILE=log.txt
+parallel --halt now,fail=1 --jobs 4 --no-run-if-empty "
+  $PSQL -c \"
+  SET log_statement = 'all';
+SELECT bcfishpass.load_dnstr(
+  'bcfishpass.streams',
+  'segmented_stream_id',
+  'bcfishpass.barriers_{1}',
+  'barriers_{1}_id',
+  'bcfishpass.streams_dnstr_barriers',
+  'barriers_{1}_dnstr',
+  'true',
+  '{2}'
+);\" 
+" ::: $MODELS ::: $WSGS
 
 # record observations downstream
 # (for convenience for field investigation and reporting, not as input into the individual models)
 $PSQL -c "truncate bcfishpass.streams_upstr_observations"
-$PARALLEL $PSQL -f sql/load_streams_upstr_observations.sql -v wsg={1} ::: $WSGS
+parallel --halt now,fail=1 --jobs 4 --no-run-if-empty \
+  $PSQL -f sql/load_streams_upstr_observations.sql -v wsg={1} ::: $WSGS
+
 $PSQL -c "truncate bcfishpass.streams_dnstr_species"
-$PARALLEL $PSQL -f sql/load_streams_dnstr_species.sql -v wsg={1} ::: $WSGS
+parallel --halt now,fail=1 --jobs 4 --no-run-if-empty \
+  $PSQL -f sql/load_streams_dnstr_species.sql -v wsg={1} ::: $WSGS
