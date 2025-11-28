2fcfc4b3c0f46511e63e8f543920987e405eafa4
Simon Norris <snorris@hillcrestgeo.ca>
2025-11-27 16:53:08 -0800

    truncate the table before loading

--- model/01_access/sql/load_crossings_upstr_observations.sql
+++ model/01_access/sql/load_crossings_upstr_observations.sql
@@ -1,30 +1,36 @@
 -- upstream observations ***within the same watershed group***
-insert into bcfishpass.crossings_upstr_observations (
-  aggregated_crossings_id,
-  observedspp_upstr
-)
+BEGIN;
 
-select
-  aggregated_crossings_id,
-  array_agg(species_code) as observedspp_upstr
-FROM
-  (
-    select distinct
-      a.aggregated_crossings_id,
-      unnest(species_codes) as species_code
-    from bcfishpass.crossings a
-    left outer join bcfishobs.fiss_fish_obsrvtn_events fo
-    on FWA_Upstream(
-      a.blue_line_key,
-      a.downstream_route_measure,
-      a.wscode_ltree,
-      a.localcode_ltree,
-      fo.blue_line_key,
-      fo.downstream_route_measure,
-      fo.wscode_ltree,
-      fo.localcode_ltree
-     )
-    and a.watershed_group_code = fo.watershed_group_code
-    order by a.aggregated_crossings_id, species_code
-  ) AS f
-group by aggregated_crossings_id;
+  truncate bcfishpass.crossings_upstr_observations;
+
+  insert into bcfishpass.crossings_upstr_observations (
+    aggregated_crossings_id,
+    observedspp_upstr
+  )
+
+  select
+    aggregated_crossings_id,
+    array_agg(species_code) as observedspp_upstr
+  FROM
+    (
+      select distinct
+        a.aggregated_crossings_id,
+        unnest(species_codes) as species_code
+      from bcfishpass.crossings a
+      left outer join bcfishobs.fiss_fish_obsrvtn_events fo
+      on FWA_Upstream(
+        a.blue_line_key,
+        a.downstream_route_measure,
+        a.wscode_ltree,
+        a.localcode_ltree,
+        fo.blue_line_key,
+        fo.downstream_route_measure,
+        fo.wscode_ltree,
+        fo.localcode_ltree
+       )
+      and a.watershed_group_code = fo.watershed_group_code
+      order by a.aggregated_crossings_id, species_code
+    ) AS f
+  group by aggregated_crossings_id;
+
+COMMIT;
