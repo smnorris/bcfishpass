0e71ef77038bcbd570a236d893a5b2717464381d
Simon Norris <snorris@hillcrestgeo.ca>
2025-12-24 11:22:35 -0800

    add view for the rough resident spp model

--- db/v0.7.7/migrate.sh
+++ db/v0.7.7/migrate.sh
@@ -5,4 +5,8 @@ PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 
 # rename/clean up cabd fix tables and permit additions/exlusions/barrier status updates
 $PSQL -f cabd_fix_tables.sql
+
+# add a rough resident species view
+$PSQL -f streams_ct_dv_rb.sql
+
 $PSQL -c "update bcfishpass.db_version set tag = '${PWD##*/}'"
