6e386f5966c2950321468d9a158a47a2055a3ec6
Simon Norris <snorris@hillcrestgeo.ca>
2026-01-27 13:03:14 +0900

    load all gradient barriers

--- model/01_access/gradient_barriers/gradient_barriers.sh
+++ model/01_access/gradient_barriers/gradient_barriers.sh
@@ -2,8 +2,7 @@
 set -euxo pipefail
 
 PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
-WSGS=$($PSQL -AXt -c "SELECT watershed_group_code FROM bcfishpass.parameters_habitat_method WHERE watershed_group_code IN
- ('VICT','COWN','SANJ')")
+WSGS=$($PSQL -AXt -c "SELECT watershed_group_code FROM bcfishpass.parameters_habitat_method")
 
 # load
 parallel $PSQL -f sql/gradient_barriers_load.sql -v wsg={1} ::: $WSGS
