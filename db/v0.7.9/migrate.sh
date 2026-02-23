f927eb5454f47bfb5a7237b9afb9bc2868ba5329
Simon Norris <snorris@hillcrestgeo.ca>
2026-02-23 12:44:55 -0800

    bump db version

--- db/v0.7.9/migrate.sh
+++ db/v0.7.9/migrate.sh
@@ -5,3 +5,6 @@ PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 
 # temp fix to exclude certain PSCIS crossings from bcfishpass.crossings_wcrp_vw
 $PSQL -f pscis_exclusions.sql
+
+# bump db version
+$PSQL -c "update bcfishpass.db_version set tag = '${PWD##*/}'"
