260b7ce53f185de055f2ffa03fd15f1585c9411e
Simon Norris <snorris@hillcrestgeo.ca>
2026-03-05 21:38:52 -0800

    update elevation barrier heights

--- model/01_access/elevation_barriers/load_elevation_barriers.sql
+++ model/01_access/elevation_barriers/load_elevation_barriers.sql
@@ -16,25 +16,47 @@ BEGIN;
     watershed_group_code,
     elevation
   )
-  WITH z1000 as
+  WITH z688 as
   (
     SELECT
       blue_line_key,
       wscode,
       localcode,
       watershed_group_code,
-      (st_dump(st_locatebetweenelevations(geom, 1000, 1001))).geom as geom
+      (st_dump(st_locatebetweenelevations(geom, 688, 689))).geom as geom
     FROM whse_basemapping.fwa_streams
   ),
 
-  z1500 as
+  z1154 as
   (
     SELECT
       blue_line_key,
       wscode,
       localcode,
       watershed_group_code,
-      (st_dump(st_locatebetweenelevations(geom, 1500, 1501))).geom as geom
+      (st_dump(st_locatebetweenelevations(geom, 1154, 1155))).geom as geom
+    FROM whse_basemapping.fwa_streams
+  ),
+
+  z1450 as
+  (
+    SELECT
+      blue_line_key,
+      wscode,
+      localcode,
+      watershed_group_code,
+      (st_dump(st_locatebetweenelevations(geom, 1450, 1451))).geom as geom
+    FROM whse_basemapping.fwa_streams
+  ),
+
+  z1466 as
+  (
+    SELECT
+      blue_line_key,
+      wscode,
+      localcode,
+      watershed_group_code,
+      (st_dump(st_locatebetweenelevations(geom, 1466, 1467))).geom as geom
     FROM whse_basemapping.fwa_streams
   )
 
@@ -52,9 +74,9 @@ BEGIN;
     wscode as wscode_ltree,
     localcode as localcode_ltree,
     watershed_group_code,
-    1000 as elevation
-  FROM z1000
-  WHERE st_z(st_startpoint(geom)) = 1000
+    688 as elevation
+  FROM z688
+  WHERE st_z(st_startpoint(geom)) = 688
   UNION ALL
   SELECT
     blue_line_key,
@@ -62,9 +84,29 @@ BEGIN;
     wscode as wscode_ltree,
     localcode as localcode_ltree,
     watershed_group_code,
-    1500 as elevation
-  FROM z1500
-  WHERE st_z(st_startpoint(geom)) = 1500
+    1154 as elevation
+  FROM z1154
+  WHERE st_z(st_startpoint(geom)) = 1154
+  UNION ALL
+  SELECT
+    blue_line_key,
+    round(st_m(st_startpoint(geom))::numeric)::integer as downstream_route_measure,
+    wscode as wscode_ltree,
+    localcode as localcode_ltree,
+    watershed_group_code,
+    1450 as elevation
+  FROM z1450
+  WHERE st_z(st_startpoint(geom)) = 1450
+  UNION ALL
+  SELECT
+    blue_line_key,
+    round(st_m(st_startpoint(geom))::numeric)::integer as downstream_route_measure,
+    wscode as wscode_ltree,
+    localcode as localcode_ltree,
+    watershed_group_code,
+    1466 as elevation
+  FROM z1466
+  WHERE st_z(st_startpoint(geom)) = 1466
   ) as f
   ORDER BY blue_line_key, downstream_route_measure;
 
