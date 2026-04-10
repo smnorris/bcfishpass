b5758d55d272227b3de1e25e62fe76f03c4e3168
Simon Norris <snorris@hillcrestgeo.ca>
2026-04-10 14:39:32 -0700

    allow reporting on modelled habitat on mining altered streams

--- model/02_habitat_linear/sql/load_habitat_linear.sql
+++ model/02_habitat_linear/sql/load_habitat_linear.sql
@@ -30,7 +30,8 @@ begin;
   select
     s.segmented_stream_id,
     case
-      when coalesce(u.spawning_bt, 0) = -4 then -4
+      when coalesce(bt.spawning, false) is false and coalesce(u.spawning_bt, 0) = -4 then -4
+      when coalesce(bt.spawning, false) is true and coalesce(u.spawning_bt, 0) = -4 then -4
       when coalesce(u.spawning_bt, 0) = -1 then -1
       when coalesce(bt.spawning, false) is true and coalesce(u.spawning_bt, 0) = 0 then 1
       when coalesce(bt.spawning, false) is true and coalesce(u.spawning_bt, 0) = 1 then 2
@@ -38,7 +39,8 @@ begin;
       else 0
     end as spawning_bt,
     case
-      when coalesce(u.spawning_ch, 0) = -4 then -4
+      when coalesce(ch.spawning, false) is false and coalesce(u.spawning_ch, 0) = -4 then -4
+      when coalesce(ch.spawning, false) is true and coalesce(u.spawning_ch, 0) = -4 then -5
       when coalesce(u.spawning_ch, 0) = -1 then -1
       when coalesce(ch.spawning, false) is true and coalesce(u.spawning_ch, 0) = 0 then 1
       when coalesce(ch.spawning, false) is true and coalesce(u.spawning_ch, 0) = 1 then 2
@@ -46,7 +48,8 @@ begin;
       else 0
     end as spawning_ch,
     case
-      when coalesce(u.spawning_cm, 0) = -4 then -4
+      when coalesce(cm.spawning, false) is false and coalesce(u.spawning_cm, 0) = -4 then -4
+      when coalesce(cm.spawning, false) is true and coalesce(u.spawning_cm, 0) = -4 then -5
       when coalesce(u.spawning_cm, 0) = -1 then -1
       when coalesce(cm.spawning, false) is true and coalesce(u.spawning_cm, 0) = 0 then 1
       when coalesce(cm.spawning, false) is true and coalesce(u.spawning_cm, 0) = 1 then 2
@@ -54,7 +57,8 @@ begin;
       else 0
     end as spawning_cm,
     case
-      when coalesce(u.spawning_co, 0) = -4 then -4
+      when coalesce(co.spawning, false) is false and coalesce(u.spawning_co, 0) = -4 then -4
+      when coalesce(co.spawning, false) is true and coalesce(u.spawning_co, 0) = -4 then -5
       when coalesce(u.spawning_co, 0) = -1 then -1
       when coalesce(co.spawning, false) is true and coalesce(u.spawning_co, 0) = 0 then 1
       when coalesce(co.spawning, false) is true and coalesce(u.spawning_co, 0) = 1 then 2
@@ -62,7 +66,8 @@ begin;
       else 0
     end as spawning_co,
     case
-      when coalesce(u.spawning_pk, 0) = -4 then -4
+      when coalesce(pk.spawning, false) is false and coalesce(u.spawning_pk, 0) = -4 then -4
+      when coalesce(pk.spawning, false) is true and coalesce(u.spawning_pk, 0) = -4 then -5
       when coalesce(u.spawning_pk, 0) = -1 then -1
       when coalesce(pk.spawning, false) is true and coalesce(u.spawning_pk, 0) = 0 then 1
       when coalesce(pk.spawning, false) is true and coalesce(u.spawning_pk, 0) = 1 then 2
@@ -70,7 +75,8 @@ begin;
       else 0
     end as spawning_pk,
     case
-      when coalesce(u.spawning_sk, 0) = -4 then -4
+      when coalesce(sk.spawning, false) is false and coalesce(u.spawning_sk, 0) = -4 then -4
+      when coalesce(sk.spawning, false) is true and coalesce(u.spawning_sk, 0) = -4 then -5
       when coalesce(u.spawning_sk, 0) = -1 then -1
       when coalesce(sk.spawning, false) is true and coalesce(u.spawning_sk, 0) = 0 then 1
       when coalesce(sk.spawning, false) is true and coalesce(u.spawning_sk, 0) = 1 then 2
@@ -78,7 +84,8 @@ begin;
       else 0
     end as spawning_sk,
     case
-      when coalesce(u.spawning_st, 0) = -4 then -4
+      when coalesce(st.spawning, false) is false and coalesce(u.spawning_st, 0) = -4 then -4
+      when coalesce(st.spawning, false) is true and coalesce(u.spawning_st, 0) = -4 then -5
       when coalesce(u.spawning_st, 0) = -1 then -1
       when coalesce(st.spawning, false) is true and coalesce(u.spawning_st, 0) = 0 then 1
       when coalesce(st.spawning, false) is true and coalesce(u.spawning_st, 0) = 1 then 2
@@ -86,7 +93,8 @@ begin;
       else 0
     end as spawning_st,
     case
-      when coalesce(u.spawning_wct, 0) = -4 then -4
+      when coalesce(wct.spawning, false) is false and coalesce(u.spawning_wct, 0) = -4 then -4
+      when coalesce(wct.spawning, false) is true and coalesce(u.spawning_wct, 0) = -4 then -5
       when coalesce(u.spawning_wct, 0) = -1 then -1
       when coalesce(wct.spawning, false) is true and coalesce(u.spawning_wct, 0) = 0 then 1
       when coalesce(wct.spawning, false) is true and coalesce(u.spawning_wct, 0) = 1 then 2
@@ -94,7 +102,8 @@ begin;
       else 0
     end as spawning_wct,
     case
-      when coalesce(u.rearing_bt, 0) = -4 then -4
+      when coalesce(bt.rearing, false) is false and coalesce(u.rearing_bt, 0) = -4 then -4
+      when coalesce(bt.rearing, false) is true and coalesce(u.rearing_bt, 0) = -4 then -5
       when coalesce(u.rearing_bt, 0) = -1 then -1
       when coalesce(bt.rearing, false) is true and coalesce(u.rearing_bt, 0) = 0 then 1
       when coalesce(bt.rearing, false) is true and coalesce(u.rearing_bt, 0) = 1 then 2
@@ -102,7 +111,8 @@ begin;
       else 0
     end as rearing_bt,
     case
-      when coalesce(u.rearing_ch, 0) = -4 then -4
+      when coalesce(ch.rearing, false) is false and coalesce(u.rearing_ch, 0) = -4 then -4
+      when coalesce(ch.rearing, false) is true and coalesce(u.rearing_ch, 0) = -4 then -5
       when coalesce(u.rearing_ch, 0) = -1 then -1
       when coalesce(ch.rearing, false) is true and coalesce(u.rearing_ch, 0) = 0 then 1
       when coalesce(ch.rearing, false) is true and coalesce(u.rearing_ch, 0) = 1 then 2
@@ -110,7 +120,8 @@ begin;
       else 0
     end as rearing_ch,
     case
-      when coalesce(u.rearing_co, 0) = -4 then -4
+      when coalesce(co.rearing, false) is false and coalesce(u.rearing_co, 0) = -4 then -4
+      when coalesce(co.rearing, false) is true and coalesce(u.rearing_co, 0) = -4 then -5
       when coalesce(u.rearing_co, 0) = -1 then -1
       when coalesce(co.rearing, false) is true and coalesce(u.rearing_co, 0) = 0 then 1
       when coalesce(co.rearing, false) is true and coalesce(u.rearing_co, 0) = 1 then 2
@@ -118,7 +129,8 @@ begin;
       else 0
     end as rearing_co,
     case
-      when coalesce(u.rearing_sk, 0) = -4 then -4
+      when coalesce(sk.rearing, false) is false and coalesce(u.rearing_sk, 0) = -4 then -4
+      when coalesce(sk.rearing, false) is true and coalesce(u.rearing_sk, 0) = -4 then -5
       when coalesce(u.rearing_sk, 0) = -1 then -1
       when coalesce(sk.rearing, false) is true and coalesce(u.rearing_sk, 0) = 0 then 1
       when coalesce(sk.rearing, false) is true and coalesce(u.rearing_sk, 0) = 1 then 2
@@ -126,7 +138,8 @@ begin;
       else 0
     end as rearing_sk,
     case
-      when coalesce(u.rearing_st, 0) = -4 then -4
+      when coalesce(st.rearing, false) is false and coalesce(u.rearing_st, 0) = -4 then -4
+      when coalesce(st.rearing, false) is true and coalesce(u.rearing_st, 0) = -4 then -5
       when coalesce(u.rearing_st, 0) = -1 then -1
       when coalesce(st.rearing, false) is true and coalesce(u.rearing_st, 0) = 0 then 1
       when coalesce(st.rearing, false) is true and coalesce(u.rearing_st, 0) = 1 then 2
@@ -134,7 +147,8 @@ begin;
       else 0
     end as rearing_st,
     case
-      when coalesce(u.rearing_wct, 0) = -4 then -4
+      when coalesce(wct.rearing, false) is false and coalesce(u.rearing_wct, 0) = -4 then -4
+      when coalesce(wct.rearing, false) is true and coalesce(u.rearing_wct, 0) = -4 then -5
       when coalesce(u.rearing_wct, 0) = -1 then -1
       when coalesce(wct.rearing, false) is true and coalesce(u.rearing_wct, 0) = 0 then 1
       when coalesce(wct.rearing, false) is true and coalesce(u.rearing_wct, 0) = 1 then 2
