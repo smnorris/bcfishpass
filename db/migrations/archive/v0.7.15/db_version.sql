697b1ec5a90d3c26df44539673e4cc53ce952544
Simon Norris <snorris@hillcrestgeo.ca>
2026-04-22 16:55:22 -0700

    correct the order of operations

--- db/migrations/archive/v0.7.15/db_version.sql
+++ db/migrations/archive/v0.7.15/db_version.sql
@@ -3,4 +3,3 @@ ALTER TABLE bcfishpass.db_version ALTER COLUMN tag SET NOT NULL;
 ALTER TABLE bcfishpass.db_version ADD COLUMN applied_at timestamp;
 UPDATE bcfishpass.db_version SET applied_at = '2026-04-16';
 ALTER TABLE bcfishpass.db_version ALTER COLUMN applied_at SET NOT NULL DEFAULT now();
-UPDATE bcfishpass.db_version SET tag = '20260415';
--- db/migrations/archive/v0.7.15/migrate.sh
+++ db/migrations/archive/v0.7.15/migrate.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 PSQL="psql $DATABASE_URL -v ON_ERROR_STOP=1"
 
 $PSQL -f db_version.sql
-$PSQL -c "update bcfishpass.db_version set tag = '${PWD##*/}', applied_at='2026-04-22'"
+$PSQL -c "update bcfishpass.db_version set tag = '20260415', applied_at='2026-04-22'"
