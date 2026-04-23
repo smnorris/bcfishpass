1d3a79dd323c138894dec5e567fb7e4817e4c09b
Simon Norris <snorris@hillcrestgeo.ca>
2026-04-23 15:23:11 -0700

    update draft pgfs function

--- db/migrations/202604230000__wcrp_habitat_connectivity_update.sql
+++ db/migrations/202604230000__wcrp_habitat_connectivity_update.sql
@@ -148,126 +148,192 @@ BEGIN;
 
 
 
--- report on total modelled habitat vs accessible modelled habitat
 DROP FUNCTION IF EXISTS postgisftw.wcrp_habitat_connectivity_status;
 
 CREATE OR REPLACE FUNCTION postgisftw.wcrp_habitat_connectivity_status(
-  wcrp TEXT
+    wcrp                      TEXT,
+    p_watershed_group_code    TEXT DEFAULT NULL
 )
 
-  RETURNS TABLE(
-   total_spawning_ch                                       numeric,
-   total_spawning_co                                       numeric,
-   total_spawning_sk                                       numeric,
-   total_spawning_st                                       numeric,
-   total_spawning_wct                                      numeric,
-   
-   total_rearing_ch                                        numeric,
-   total_rearing_co                                        numeric,
-   total_rearing_sk                                        numeric,
-   total_rearing_st                                        numeric,
-   total_rearing_wct                                       numeric,
+RETURNS TABLE(
+    watershed_group_code                                    text,
+    total_spawning_ch                                       numeric,
+    total_spawning_co                                       numeric,
+    total_spawning_sk                                       numeric,
+    total_spawning_st                                       numeric,
+    total_spawning_wct                                      numeric,
+    total_rearing_ch                                        numeric,
+    total_rearing_co                                        numeric,
+    total_rearing_sk                                        numeric,
+    total_rearing_st                                        numeric,
+    total_rearing_wct                                       numeric,
+    total_spawningrearing_ch                                numeric,
+    total_spawningrearing_co                                numeric,
+    total_spawningrearing_sk                                numeric,
+    total_spawningrearing_st                                numeric,
+    total_spawningrearing_wct                               numeric,
+    total_spawning_all                                      numeric,
+    total_rearing_all                                       numeric,
+    total_spawningrearing_all                               numeric,
 
-   total_spawningrearing_ch                                numeric,
-   total_spawningrearing_co                                numeric,
-   total_spawningrearing_sk                                numeric,
-   total_spawningrearing_st                                numeric,
-   total_spawningrearing_wct                               numeric,
+    accessible_spawning_ch                                  numeric,
+    accessible_spawning_co                                  numeric,
+    accessible_spawning_sk                                  numeric,
+    accessible_spawning_st                                  numeric,
+    accessible_spawning_wct                                 numeric,
+    accessible_rearing_ch                                   numeric,
+    accessible_rearing_co                                   numeric,
+    accessible_rearing_sk                                   numeric,
+    accessible_rearing_st                                   numeric,
+    accessible_rearing_wct                                  numeric,
+    accessible_spawningrearing_ch                           numeric,
+    accessible_spawningrearing_co                           numeric,
+    accessible_spawningrearing_sk                           numeric,
+    accessible_spawningrearing_st                           numeric,
+    accessible_spawningrearing_wct                          numeric,
+    accessible_spawning_all                                 numeric,
+    accessible_rearing_all                                  numeric,
+    accessible_spawningrearing_all                          numeric,
 
-   total_spawning_all                                      numeric,
-   total_rearing_all                                       numeric,
-   total_spawningrearing_all                               numeric,
+    disconnected_spawning_ch                                numeric,
+    disconnected_spawning_co                                numeric,
+    disconnected_spawning_sk                                numeric,
+    disconnected_spawning_st                                numeric,
+    disconnected_spawning_wct                               numeric,
+    disconnected_rearing_ch                                 numeric,
+    disconnected_rearing_co                                 numeric,
+    disconnected_rearing_sk                                 numeric,
+    disconnected_rearing_st                                 numeric,
+    disconnected_rearing_wct                                numeric,
+    disconnected_spawningrearing_ch                         numeric,
+    disconnected_spawningrearing_co                         numeric,
+    disconnected_spawningrearing_sk                         numeric,
+    disconnected_spawningrearing_st                         numeric,
+    disconnected_spawningrearing_wct                        numeric,
+    disconnected_spawning_all                               numeric,
+    disconnected_rearing_all                                numeric,
+    disconnected_spawningrearing_all                        numeric,
 
-   accessible_spawning_ch                                       numeric,
-   accessible_spawning_co                                       numeric,
-   accessible_spawning_sk                                       numeric,
-   accessible_spawning_st                                       numeric,
-   accessible_spawning_wct                                      numeric,
-   
-   accessible_rearing_ch                                        numeric,
-   accessible_rearing_co                                        numeric,
-   accessible_rearing_sk                                        numeric,
-   accessible_rearing_st                                        numeric,
-   accessible_rearing_wct                                       numeric,
+    pct_accessible_spawning_ch                              numeric,
+    pct_accessible_spawning_co                              numeric,
+    pct_accessible_spawning_sk                              numeric,
+    pct_accessible_spawning_st                              numeric,
+    pct_accessible_spawning_wct                             numeric,
+    pct_accessible_rearing_ch                               numeric,
+    pct_accessible_rearing_co                               numeric,
+    pct_accessible_rearing_sk                               numeric,
+    pct_accessible_rearing_st                               numeric,
+    pct_accessible_rearing_wct                              numeric,
+    pct_accessible_spawningrearing_ch                       numeric,
+    pct_accessible_spawningrearing_co                       numeric,
+    pct_accessible_spawningrearing_sk                       numeric,
+    pct_accessible_spawningrearing_st                       numeric,
+    pct_accessible_spawningrearing_wct                      numeric,
+    pct_accessible_spawning_all                             numeric,
+    pct_accessible_rearing_all                              numeric,
+    pct_accessible_spawningrearing_all                      numeric
+)
 
-   accessible_spawningrearing_ch                                numeric,
-   accessible_spawningrearing_co                                numeric,
-   accessible_spawningrearing_sk                                numeric,
-   accessible_spawningrearing_st                                numeric,
-   accessible_spawningrearing_wct                               numeric,
-
-   accessible_spawning_all                                      numeric,
-   accessible_rearing_all                                       numeric,
-   accessible_spawningrearing_all                               numeric
-  )
-
-  LANGUAGE 'plpgsql'
-  IMMUTABLE PARALLEL SAFE
+LANGUAGE 'plpgsql'
+STABLE PARALLEL SAFE
 
 AS $$
 
 DECLARE
-   v_wcrp  text := wcrp;
+    v_wcrp                 text := wcrp;
+    v_watershed_group_code text := p_watershed_group_code;
 
 BEGIN
 RETURN QUERY
-select
-   v.total_spawning_ch                                       numeric,
-   v.total_spawning_co                                       numeric,
-   v.total_spawning_sk                                       numeric,
-   v.total_spawning_st                                       numeric,
-   v.total_spawning_wct                                      numeric,
-
-   v.total_rearing_ch                                        numeric,
-   v.total_rearing_co                                        numeric,
-   v.total_rearing_sk                                        numeric,
-   v.total_rearing_st                                        numeric,
-   v.total_rearing_wct                                       numeric,
-
-   v.total_spawningrearing_ch                                numeric,
-   v.total_spawningrearing_co                                numeric,
-   v.total_spawningrearing_sk                                numeric,
-   v.total_spawningrearing_st                                numeric,
-   v.total_spawningrearing_wct                               numeric,
-
-   v.total_spawning_all                                      numeric,
-   v.total_rearing_all                                       numeric,
-   v.total_spawningrearing_all                               numeric,
-
-   v.accessible_spawning_ch                                       numeric,
-   v.accessible_spawning_co                                       numeric,
-   v.accessible_spawning_sk                                       numeric,
-   v.accessible_spawning_st                                       numeric,
-   v.accessible_spawning_wct                                      numeric,
-
-   v.accessible_rearing_ch                                        numeric,
-   v.accessible_rearing_co                                        numeric,
-   v.accessible_rearing_sk                                        numeric,
-   v.accessible_rearing_st                                        numeric,
-   v.accessible_rearing_wct                                       numeric,
-
-   v.accessible_spawningrearing_ch                                numeric,
-   v.accessible_spawningrearing_co                                numeric,
-   v.accessible_spawningrearing_sk                                numeric,
-   v.accessible_spawningrearing_st                                numeric,
-   v.accessible_spawningrearing_wct                               numeric,
-
-   v.accessible_spawning_all                                      numeric,
-   v.accessible_rearing_all                                       numeric,
-   v.accessible_spawningrearing_all                               numeric
-from bcfishpass.wcrp_habitat_connectivity_status_vw v
-where v.wcrp = v_wcrp;
+SELECT
+    v.watershed_group_code,
+    v.total_spawning_ch,
+    v.total_spawning_co,
+    v.total_spawning_sk,
+    v.total_spawning_st,
+    v.total_spawning_wct,
+    v.total_rearing_ch,
+    v.total_rearing_co,
+    v.total_rearing_sk,
+    v.total_rearing_st,
+    v.total_rearing_wct,
+    v.total_spawningrearing_ch,
+    v.total_spawningrearing_co,
+    v.total_spawningrearing_sk,
+    v.total_spawningrearing_st,
+    v.total_spawningrearing_wct,
+    v.total_spawning_all,
+    v.total_rearing_all,
+    v.total_spawningrearing_all,
+    v.accessible_spawning_ch,
+    v.accessible_spawning_co,
+    v.accessible_spawning_sk,
+    v.accessible_spawning_st,
+    v.accessible_spawning_wct,
+    v.accessible_rearing_ch,
+    v.accessible_rearing_co,
+    v.accessible_rearing_sk,
+    v.accessible_rearing_st,
+    v.accessible_rearing_wct,
+    v.accessible_spawningrearing_ch,
+    v.accessible_spawningrearing_co,
+    v.accessible_spawningrearing_sk,
+    v.accessible_spawningrearing_st,
+    v.accessible_spawningrearing_wct,
+    v.accessible_spawning_all,
+    v.accessible_rearing_all,
+    v.accessible_spawningrearing_all,
+    v.disconnected_spawning_ch,
+    v.disconnected_spawning_co,
+    v.disconnected_spawning_sk,
+    v.disconnected_spawning_st,
+    v.disconnected_spawning_wct,
+    v.disconnected_rearing_ch,
+    v.disconnected_rearing_co,
+    v.disconnected_rearing_sk,
+    v.disconnected_rearing_st,
+    v.disconnected_rearing_wct,
+    v.disconnected_spawningrearing_ch,
+    v.disconnected_spawningrearing_co,
+    v.disconnected_spawningrearing_sk,
+    v.disconnected_spawningrearing_st,
+    v.disconnected_spawningrearing_wct,
+    v.disconnected_spawning_all,
+    v.disconnected_rearing_all,
+    v.disconnected_spawningrearing_all,
+    v.pct_accessible_spawning_ch,
+    v.pct_accessible_spawning_co,
+    v.pct_accessible_spawning_sk,
+    v.pct_accessible_spawning_st,
+    v.pct_accessible_spawning_wct,
+    v.pct_accessible_rearing_ch,
+    v.pct_accessible_rearing_co,
+    v.pct_accessible_rearing_sk,
+    v.pct_accessible_rearing_st,
+    v.pct_accessible_rearing_wct,
+    v.pct_accessible_spawningrearing_ch,
+    v.pct_accessible_spawningrearing_co,
+    v.pct_accessible_spawningrearing_sk,
+    v.pct_accessible_spawningrearing_st,
+    v.pct_accessible_spawningrearing_wct,
+    v.pct_accessible_spawning_all,
+    v.pct_accessible_rearing_all,
+    v.pct_accessible_spawningrearing_all
+FROM bcfishpass.wcrp_habitat_connectivity_status_vw v
+WHERE v.wcrp = v_wcrp
+AND (v_watershed_group_code IS NULL OR v.watershed_group_code = v_watershed_group_code);
 
 END
 $$;
 
 COMMENT ON FUNCTION postgisftw.wcrp_habitat_connectivity_status IS
 'For WCRP specified, return total/accessible lengths and percentage accessible for habitat types spawning; rearing; spawning and/or rearing 
-per target species and for all target species.';
+per target species and for all target species. Optionally filter by watershed_group_code.';
 
 REVOKE EXECUTE ON FUNCTION postgisftw.wcrp_habitat_connectivity_status FROM public;
 
 -- select * from postgisftw.wcrp_habitat_connectivity_status('takla')
+-- select * from postgisftw.wcrp_habitat_connectivity_status('takla','DRIR')
 -- select * from postgisftw.wcrp_habitat_connectivity_status('hors')
 -- select * from postgisftw.wcrp_habitat_connectivity_status('bowr_ques_carr')
 
