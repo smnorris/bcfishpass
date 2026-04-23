d4caf14506a2c66e8e576ff9c8a6bd99939c561b
Simon Norris <snorris@hillcrestgeo.ca>
2026-04-23 13:52:59 -0700

    fix validate script

--- wcrp/scripts/validate_config.py
+++ wcrp/scripts/validate_config.py
@@ -1,17 +1,16 @@
 from pathlib import Path
 import json
 import jsonschema
+import os
 import psycopg2
+import sys
 import yaml
 
-base = Path(__file__).parent.parent 	
+base = Path(__file__).parent.parent
 config = yaml.safe_load(open(base / 'plan_config.yaml'))
 schema = json.load(open(base / 'plan_config.schema.json'))
-
-# is schema valid json?
 jsonschema.validate(config, schema)
 
-# does the provided query return any stream data?
 conn = psycopg2.connect(os.environ['DATABASE_URL'])
 cur = conn.cursor()
 
