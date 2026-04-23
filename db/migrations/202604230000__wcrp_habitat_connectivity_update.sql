0f5ec0320cd9e4a35beacbcfe0a42205328ca731
Simon Norris <snorris@hillcrestgeo.ca>
2026-04-23 15:05:52 -0700

    add pk and ensure pct accessible columns are null for non-target species

--- db/migrations/202604230000__wcrp_habitat_connectivity_update.sql
+++ db/migrations/202604230000__wcrp_habitat_connectivity_update.sql
@@ -51,9 +51,10 @@ BEGIN;
 
    accessible_spawning_all                                      numeric,
    accessible_rearing_all                                       numeric,
-   accessible_spawningrearing_all                               numeric
-
-  );
+   accessible_spawningrearing_all                               numeric,
+  
+   primary key (model_run_id, wcrp, watershed_group_code)
+);
 
 
   -- view of current habitat connecitvity status, plus percentages
@@ -121,25 +122,25 @@ BEGIN;
     total_rearing_all - accessible_rearing_all as disconnected_rearing_all,
     total_spawningrearing_all - accessible_spawningrearing_all as disconnected_spawningrearing_all,
 
-    -- percentage accessible
-    round(coalesce(accessible_spawning_ch / nullif(total_spawning_ch, 0),0) * 100, 2) as pct_accessible_spawning_ch,
-    round(coalesce(accessible_spawning_co / nullif(total_spawning_co, 0),0) * 100, 2) as pct_accessible_spawning_co,
-    round(coalesce(accessible_spawning_sk / nullif(total_spawning_sk, 0),0) * 100, 2) as pct_accessible_spawning_sk,
-    round(coalesce(accessible_spawning_st / nullif(total_spawning_st, 0),0) * 100, 2) as pct_accessible_spawning_st,
-    round(coalesce(accessible_spawning_wct / nullif(total_spawning_wct, 0),0) * 100, 2) as pct_accessible_spawning_wct,
-    round(coalesce(accessible_rearing_ch / nullif(total_rearing_ch, 0),0) * 100, 2) as pct_accessible_rearing_ch,
-    round(coalesce(accessible_rearing_co / nullif(total_rearing_co, 0),0) * 100, 2) as pct_accessible_rearing_co,
-    round(coalesce(accessible_rearing_sk / nullif(total_rearing_sk, 0),0) * 100, 2) as pct_accessible_rearing_sk,
-    round(coalesce(accessible_rearing_st / nullif(total_rearing_st, 0),0) * 100, 2) as pct_accessible_rearing_st,
-    round(coalesce(accessible_rearing_wct / nullif(total_rearing_wct, 0),0) * 100, 2) as pct_accessible_rearing_wct,
-    round(coalesce(accessible_spawningrearing_ch / nullif(total_spawningrearing_ch, 0),0) * 100, 2) as pct_accessible_spawningrearing_ch,
-    round(coalesce(accessible_spawningrearing_co / nullif(total_spawningrearing_co, 0),0) * 100, 2) as pct_accessible_spawningrearing_co,
-    round(coalesce(accessible_spawningrearing_sk / nullif(total_spawningrearing_sk, 0),0) * 100, 2) as pct_accessible_spawningrearing_sk,
-    round(coalesce(accessible_spawningrearing_st / nullif(total_spawningrearing_st, 0),0) * 100, 2) as pct_accessible_spawningrearing_st,
-    round(coalesce(accessible_spawningrearing_wct / nullif(total_spawningrearing_wct, 0),0) * 100, 2) as pct_accessible_spawningrearing_wct,
-    round(coalesce(accessible_spawning_all / nullif(total_spawning_all, 0),0) * 100, 2) as pct_accessible_spawning_all,
-    round(coalesce(accessible_rearing_all / nullif(total_rearing_all, 0),0) * 100, 2) as pct_accessible_rearing_all,
-    round(coalesce(accessible_spawningrearing_all / nullif(total_spawningrearing_all, 0),0) * 100, 2) as pct_accessible_spawningrearing_all
+    -- percentage accessible. null if species is not targeted
+    round(accessible_spawning_ch / nullif(total_spawning_ch, 0) * 100, 2) as pct_accessible_spawning_ch,
+    round(accessible_spawning_co / nullif(total_spawning_co, 0) * 100, 2) as pct_accessible_spawning_co,
+    round(accessible_spawning_sk / nullif(total_spawning_sk, 0) * 100, 2) as pct_accessible_spawning_sk,
+    round(accessible_spawning_st / nullif(total_spawning_st, 0) * 100, 2) as pct_accessible_spawning_st,
+    round(accessible_spawning_wct / nullif(total_spawning_wct, 0) * 100, 2) as pct_accessible_spawning_wct,
+    round(accessible_rearing_ch / nullif(total_rearing_ch, 0) * 100, 2) as pct_accessible_rearing_ch,
+    round(accessible_rearing_co / nullif(total_rearing_co, 0) * 100, 2) as pct_accessible_rearing_co,
+    round(accessible_rearing_sk / nullif(total_rearing_sk, 0) * 100, 2) as pct_accessible_rearing_sk,
+    round(accessible_rearing_st / nullif(total_rearing_st, 0) * 100, 2) as pct_accessible_rearing_st,
+    round(accessible_rearing_wct / nullif(total_rearing_wct, 0) * 100, 2) as pct_accessible_rearing_wct,
+    round(accessible_spawningrearing_ch / nullif(total_spawningrearing_ch, 0) * 100, 2) as pct_accessible_spawningrearing_ch,
+    round(accessible_spawningrearing_co / nullif(total_spawningrearing_co, 0) * 100, 2) as pct_accessible_spawningrearing_co,
+    round(accessible_spawningrearing_sk / nullif(total_spawningrearing_sk, 0) * 100, 2) as pct_accessible_spawningrearing_sk,
+    round(accessible_spawningrearing_st / nullif(total_spawningrearing_st, 0) * 100, 2) as pct_accessible_spawningrearing_st,
+    round(accessible_spawningrearing_wct / nullif(total_spawningrearing_wct, 0) * 100, 2) as pct_accessible_spawningrearing_wct,
+    round(accessible_spawning_all / nullif(total_spawning_all, 0) * 100, 2) as pct_accessible_spawning_all,
+    round(accessible_rearing_all / nullif(total_rearing_all, 0) * 100, 2) as pct_accessible_rearing_all,
+    round(accessible_spawningrearing_all / nullif(total_spawningrearing_all, 0) * 100, 2) as pct_accessible_spawningrearing_all
     
   from bcfishpass.log_wcrp_habitat_connectivity s
   inner join bcfishpass.log l on s.model_run_id = l.model_run_id
