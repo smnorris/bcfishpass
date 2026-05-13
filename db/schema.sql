cfc58011efe37ff0b9054242edd4b00d68b23892
Simon Norris <snorris@hillcrestgeo.ca>
2026-05-13 13:55:35 -0700

    add latest migration tag

--- db/schema.sql
+++ db/schema.sql
@@ -17191,3 +17191,6 @@ ALTER TABLE ONLY bcfishpass.user_habitat_classification
 -- PostgreSQL database dump complete
 --
 
+
+-- db version, appended at dump time
+INSERT INTO bcfishpass.db_version (tag, applied_at) VALUES ('202604291339', now());
